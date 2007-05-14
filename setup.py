0b955f989aec9e8d25e05b4231ed137cc648c631
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:34:07 +0000

    * Updated for Traits 2.1

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ ctraits = Extension(
     'enthought.traits.ctraits',
     sources=['enthought/traits/ctraits.c'],
 )
+speedups = Extension(
+    'enthought.traits.protocols._speedups',
+    sources=['enthought/traits/protocols/_speedups.c'],
+)
 
 setup(
     name = 'enthought.traits',
-    version = '1.1.0',
+    version = '2.1.0',
     description  = 'Explicitly typed Python attributes package',
     author       = 'David C. Morrill',
     author_email = 'dmorrill@enthought.com',
@@ -15,7 +19,7 @@ setup(
     license      = 'BSD',
     zip_safe     = False,
     packages = find_packages(),
-    ext_modules = [ctraits],
+    ext_modules = [ctraits, speedups],
     include_package_data = True,
     install_requires = [
         'numpy',
