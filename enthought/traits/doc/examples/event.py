13e0e84bc00f6f2ecfaafb97d721d81984db15fb
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-07-06 20:02:59 +0000

    * More cleanup of unused RGBAColor imports.

--- enthought/traits/doc/examples/event.py
+++ enthought/traits/doc/examples/event.py
@@ -1,6 +1,6 @@
 # event.py --- Example of trait event
 import wx
-from enthought.traits.api import Event, HasTraits, List, Tuple
+from enthought.traits.api import Event, HasTraits, List, Tuple, RGBAColor
 
 point_2d = Tuple(0, 0)
 
@@ -8,12 +8,12 @@ class Line2D(HasTraits):
     points = List(point_2d)
     line_color = RGBAColor('black')
     updated = Event
-    
+
     def redraw():
         pass # Not implemented for this example
 
     def _points_changed():
         self.updated = True
-        
+
     def _updated_fired():
         self.redraw()
