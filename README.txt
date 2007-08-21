ae88144fa7742bc9c3f21a4e59ac422c6cdb458b
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-21 16:37:11 +0000

    * Numerous fixes to the setuptools / egg-building infrastructure of the trunk packages. * Added LICESNE and README files. * Added use of the etsdep function to allow both .dev and release dependencies. * NOTE: No effort has been made to validate dependent versions yet!  We need the whole thing bulding eggs correctly first!

---
+++ README.txt
@@ -0,0 +1,2 @@
+The traits.ui.qt4 project, a part of the Enthought Tool Suite.
+
--- setup.py
+++ setup.py
@@ -1,23 +1,45 @@
 from setuptools import setup, find_packages
 
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+MODEL = etsdep('enthought.model', '2.0.0b1')
+TRAITS = etsdep('enthought.traits', '3.0.0b1')
+
+
 setup(
-    name = 'enthought.traits.ui.qt4',
-    version = '3.0.0b1',
-    description  = 'PyQt backend for enthought.traits',
-    author       = 'Phil Thompson',
+    author = 'Phil Thompson',
     author_email = 'phil@riverbankcomputing.co.uk',
-    url          = 'http://code.enthought.com/traits',
-    license      = 'GPL',
-    zip_safe     = False,
-    packages = find_packages(),
+    description = 'PyQt backend for enthought.traits',
     include_package_data = True,
     install_requires = [
         "enthought.model >=2.0.0b1, <3.0.0",
         "enthought.traits >=3.0.0b1, <4.0.0",
-    ],
+        ],
+    license = 'GPL',
+    name = 'enthought.traits.ui.qt4',
     namespace_packages = [
         "enthought",
         "enthought.traits",
         "enthought.traits.ui",
-    ],
-)
+        ],
+    packages = find_packages(),
+    url = 'http://code.enthought.com/traits',
+    version = '3.0.0b1',
+    zip_safe = False,
+    )
+
