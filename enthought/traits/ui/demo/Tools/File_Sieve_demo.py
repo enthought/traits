22a03e2b45b8b4a7ff279091c678dcde660a855f
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-11-21 00:57:14 +0000

    Added another new demo to the traits ui demo.

--- enthought/traits/ui/demo/Tools/File_Sieve_demo.py
+++ enthought/traits/ui/demo/Tools/File_Sieve_demo.py
@@ -1,14 +1,14 @@
 """
-A demonstration of the <i>Image Library Viewer</i> tool, which is part of the 
+A demonstration of the <i>File Sieve</i> tool, which is part of the 
 <b>enthought.developer</b> package.
 
 This demo is displayed as a popup window because it requires a fairly wide
 screen area in order to display all of the viewer columns. However, it can be
 embedded within any Traits UI view if desired.
 
-The top portion of the Image Library Viewer is a <i>live filter</i>, meaning
-that you can type information into any of the various fields to filter the
-set of image library image shown.
+The top portion of the File Sieve is a <i>live filter</i>, meaning that you can
+type information into any of the various fields to filter the set of files
+shown.
 
 For fields such as <i>volume<i>, the information you type can appear anywhere in
 the volume name to produce a match. The match is case insensitive.
@@ -23,24 +23,16 @@ of the viewer. If it is larger than 32x32, then the value for that cell will
 be blank. However, you can click on the cell to display a pop-up view of the
 complete image.
 
-Similarly, you can click on any <i>Copyright</i> or <i>License</i> column cell
-to display a pop-up view of the complete copyright or license information.
-
-You can also double-click on a <i>Volume</i> or <i>Name</i> column cell to
-copy the fully-qualified image library name to the system clipboard, which you
-can then paste into your Python source code to use the selected image in a
-Traits UI-based application.
-
 In order for this demo to run, you must have the enthought.developer package 
 installed.
 """
 
 try:
-    from enthought.developer.tools.image_library_viewer \
-         import ImageLibraryViewer
+    from enthought.developer.tools.file_sieve \
+         import FileSieve
     
-    # Create an instance of the Image Library Viewer as the demo to run:
-    popup = ImageLibraryViewer()
+    # Create an instance of the File Sieve as the demo to run:
+    popup = FileSieve()
 except:
     raise Exception( 'This demo requires the enthought.developer package '
                      'to be installed.' )
