fc51e148a9cfddf74d013d74ad297cf08097c6c4
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-01-07 23:50:58 +0000

    * Forgot to change the name of the current project.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires = [
         ],
     license = 'BSD',
-    name = 'enthought.traits',
+    name = 'Traits',
     namespace_packages = [
         'enthought',
         'enthought.traits',
