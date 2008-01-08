a21f644c6fd4cffe08bdb756bde8d3f052118175
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-01-08 22:39:04 +0000

    * Updated project dependencies based on a run of etscollect and etsdeps packaging tools. * Updated TODO with an urgent note about removing all dependencies ASAP! * Updated svn:ignores.

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
 from setuptools import setup, find_packages
 
 
-# NOTE: EnthoughtBase should *never* depend on another ETS project!
+# NOTE: EnthoughtBase should *never* depend on another ETS project!  We need
+# to fix the below ASAP!
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+APPTOOLS = etsdep('AppTools', '3.0.0b1')  # -- imported only in util/wx/drag_and_drop
+ENVISAGECORE = etsdep('EnvisageCore', '3.0.0b1')  # -- all from logger.(plugin|agent|widget)
+ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.0b1')  # -- all from logger.plugin
+SCIMATH = etsdep('SciMath', '3.0.0b1')  # -- all from util/wx/spreadsheet/unit_renderer.py
+TRAITS = etsdep('Traits', '3.0.0b1')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0b1')  # -- only from e.util.traits.editor.parameter_choice_editor.py
+TRAITSGUI = etsdep('TraitsGUI', '3.0.0b1')  # -- from logger.(agent|plugin|widget)
 
 
 setup(
@@ -19,7 +44,15 @@ setup(
         },
     license = 'BSD',
     include_package_data = True,
-    install_requires = [],
+    install_requires = [
+        APPTOOLS,
+        ENVISAGECORE,
+        ENVISAGEPLUGINS,
+        SCIMATH,
+        TRAITS,
+        TRAITSBACKENDWX,
+        TRAITSGUI,
+        ],
     name = 'EnthoughtBase',
     namespace_packages = [
         "enthought",
