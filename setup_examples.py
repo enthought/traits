acf1e663d1b1cd3d969dd78574eaf046335caca6
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-01-08 00:18:11 +0000

    * Updating project name to CamelCase.

--- setup_examples.py
+++ setup_examples.py
@@ -8,7 +8,7 @@ setup(
     license = 'BSD',
     packages = [],
     examples_map = 'enthought/traits/ui/demo/examples.cfg',
-    name = "traits-examples",
+    name = "TraitsExamples",
     url = 'http://code.enthought.com/traits',
     version='3.0.0b1',
     zip_safe = False,
