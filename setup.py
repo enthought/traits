e8c9c1f5fcb6e4045cefb3bb24d5bdc406bf4fa8
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-04-17 15:26:49 +0000

    Updated the setup dependencies for the core traits egg.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, Extension, find_packages
 ctraits = Extension(
     'enthought.traits.ctraits',
     sources = [ 'enthought/traits/ctraits.c' ],
-    )
+)
 
 speedups = Extension(
     'enthought.traits.protocols._speedups',
     # fixme: Use the generated sources until Pyrex 0.9.6 and setuptools can play
     # with each other. See #1364
     sources = [ 'enthought/traits/protocols/_speedups.c' ],
-    )
+)
 
 
 # Function to convert simple ETS project names and versions to a requirements
@@ -30,69 +30,94 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies:
-
-# Import of enthought.resource from traits/ui/image.py:
-APPTOOLS = etsdep('AppTools', '3.0.0b1')
-
-# from traits/trait_base.py
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b1')
-
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0b1')
-TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.0.0b1')
-TRAITSGUI_DOCK  = etsdep('TraitsGUI[dock]', '3.0.0b1')
+APPTOOLS        = etsdep( 'AppTools',        '3.0.0b1' )  
+ENTHOUGHTBASE   = etsdep( 'EnthoughtBase',   '3.0.0b1' )
+TRAITSBACKENDWX = etsdep( 'TraitsBackendWX', '3.0.0b1' )
+TRAITSBACKENDQT = etsdep( 'TraitsBackendQt', '3.0.0b1' )
+TRAITSGUI_DOCK  = etsdep( 'TraitsGUI[dock]', '3.0.0b1' )
 
 
 setup(
-    author = 'David C. Morrill',
+    author       = 'David C. Morrill',
     author_email = 'dmorrill@enthought.com',
-    description = 'Explicitly typed Python attributes package',
+    description  = 'Explicitly typed Python attributes package',
+    
     extras_require = {
-        'array': [
-            ],
+    
+        # Extra denoting that the standard ETS configuration package should be 
+        # used. If omitted, Traits will use its own package local configuration, 
+        # which is a subset of ETS Config containing just the parts used by 
+        # Traits.
+        
+        # Completely optional, not triggered by imports:
         'etsconfig': [
             ENTHOUGHTBASE,
-            ],
+        ],
+            
+        # Extra denoting that the Traits UI backend for Qt 4.0 should be 
+        # installed.
+
+        # Completely optional, not triggered by imports:
         'qt4': [
             TRAITSBACKENDQT,
-            ],
+        ],
+            
+        # The Traits UI package is always installed as part of the Traits core
+        # egg. This is an extra denoting that the Traits UI should be functional 
+        # after installation (meaning that all Traits UI modules should load 
+        # without getting any import errors). Any actual UI's created will only 
+        # work with the default 'null' backend. You must also install one of 
+        # the 'real' backends (i.e. 'qt4' or 'wx') if you actually want to 
+        # create real user interfaces:
         'ui': [
-            APPTOOLS,
-            TRAITSGUI_DOCK,
-            TRAITSBACKENDWX,
-            ],
+            TRAITSGUI_DOCK
+        ],
+            
+        # Extra denoting that the Traits UI backend for wxPython should be 
+        # installed.
+
+        # Completely optional, not triggered by imports:
         'wx': [
             TRAITSBACKENDWX,
-            ],
+        ],
+        
+        # Extra denoting that all packages needed to run the Traits unit tests
+        # should be installed:
+        'test': [
+            APPTOOLS,
+        ],
 
         # All non-ets dependencies should be in this extra to ensure users can
-        # decide whether to require them or not.
+        # decide whether to require them or not:
         'nonets': [
             'numpy >= 1.0.0',
-            ],
-        },
-    ext_modules = [ ctraits, speedups ],
-    include_package_data = True,
-    install_requires = [
         ],
-    license = 'BSD',
-    name = 'Traits',
+    },
+        
+    install_requires = [
+    ],
+        
+    ext_modules          = [ ctraits, speedups ],
+    include_package_data = True,
+    license              = 'BSD',
+    name                 = 'Traits',
     namespace_packages = [
         'enthought',
         'enthought.traits',
         'enthought.traits.ui',
-        ],
-    packages = find_packages(exclude=[
+    ],
+    packages = find_packages( exclude = [
         'docs',
         'docs.*',
         'integrationtests',
         'integrationtests.*',
-        ]),
+    ] ),
     tests_require = [
         'nose >= 0.9',
-        ],
+    ],
     test_suite = 'nose.collector',
-    url = 'http://code.enthought.com/traits',
-    version = '3.0.0b1',
-    zip_safe = False,
-    )
+    url        = 'http://code.enthought.com/traits',
+    version    = '3.0.0b1',
+    zip_safe   = False,
+)
 
