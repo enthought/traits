ab8b4f9314ffa9f4acd6e9c9cd95a0c76807c76d
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-05-13 02:47:41 +0000

    TEST: Test case for [18670].

---
+++ enthought/traits/ui/tests/large_range_editor.py
@@ -0,0 +1,39 @@
+from enthought.traits.api import HasTraits, Float, List
+from enthought.traits.ui.api import View, Item, RangeEditor
+
+# Tests the Large Range Slider editor. It also tests the case where the 
+# editor is embedded in a list.
+class TestRangeEditor(HasTraits):
+    x = Float
+    low = Float(123.123)
+    high = Float(1123.123)
+    list = List(Float(
+                     editor = RangeEditor(low_name='low', 
+                                          high_name = 'high', 
+                                          # These force the large range
+                                          # slider to be used.
+                                          low=100.0, 
+                                          high=10000.123)
+                     )
+               )
+    view = View(Item(name='x', 
+                     editor = RangeEditor(low_name='low', 
+                                          high_name = 'high', 
+                                          # These force the large range
+                                          # slider to be used.
+                                          low=100.0, 
+                                          high=10000.123)
+                     ),
+                 Item('list'),
+                 resizable=True
+                )
+
+def test():
+    a = TestRangeEditor()
+    a.x = 500
+    a.list.append(500)
+    a.edit_traits() # Just close the resulting dialog.
+    assert a.x == 500
+    assert a.list[0] == 500
+
+test()
