4cd8375724a7d3b2488ac4b19d1ebf68cf8a7dee
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-06 23:32:29 +0000

    Removed tests using defer_trait_change, which is not defined.

--- enthought/traits/tests/test_events.py
+++ enthought/traits/tests/test_events.py
@@ -15,14 +15,6 @@
 from enthought.traits.api import *
 
 #------------------------------------------------------------------------------
-# Change to True if the version of Traits under test has the defer_trait_change
-# functionality:
-#------------------------------------------------------------------------------
-
-HAVE_DEFER_TRAIT_CHANGE = False
-
-
-#------------------------------------------------------------------------------
 
 class TestBase ( HasTraits ):
     
@@ -36,37 +28,6 @@ class TestBase ( HasTraits ):
         print 'normal changes'
         self.t1 = 1
         self.t2 = 2
-
-        if ( HAVE_DEFER_TRAIT_CHANGE ) :
-            print 'deferring trait changes'
-            self.defer_trait_change()
-
-            self.t1 = 3
-            self.t2 = 4
-
-            print 'enabling trait changes'
-            self.defer_trait_change( False )
-
-            print 'making normal changes again'
-
-            self.t1 = 5
-            self.t2 = 6
-
-            print 'deferring trait changes'
-            self.defer_trait_change()
-
-            self.t1 = 7
-            self.t2 = 8
-            self.t1 = 9
-            self.t2 = 10
-
-            print 'enabling trait changes'
-            self.defer_trait_change( False )
-
-            print 'making normal changes again'
-
-            self.t1 = 11
-            self.t2 = 12
         
         print '---------- End %s test ----------\n' % self.__class__.__name__
 
@@ -167,65 +128,6 @@ class Test8 ( Test2 ):
         
 #------------------------------------------------------------------------------
 
-class Test9 ( Test2 ):
-    
-    def __init__ ( self, **traits ):
-        Test1.__init__( self, **traits )
-        print 'deferring trait changes in __init__'
-        self.defer_trait_change()
-        self.t1 = -1
-        self.t2 = -2
-        self.on_trait_change( self.t1_trait, 't1' )
-        self.on_trait_change( self.t2_trait, 't2' )
-        self.on_trait_change( self.on_anytrait )
-        self.t1 = -3
-        self.t2 = -4
-        print 'enabling trait changes in __init__'
-        self.defer_trait_change( False )
-        
-    def on_anytrait ( self, object, name, old, new ):
-        print 'on anytrait changed:', name, old, new
-        
-    def t1_trait ( self, object, name, old, new ):
-        print 'on t1 changed:', old, new
-        
-    def t2_trait ( self, object, name, old, new ):
-        print 'on t2 changed:', old, new
-        
-#------------------------------------------------------------------------------
-
-class Test10 ( Test2 ):
-    
-    def __init__ ( self, **traits ):
-        Test1.__init__( self, **traits )
-        print 'deferring trait changes in __init__'
-        self.defer_trait_change()
-        self.t1 = -1
-        self.t2 = -2
-        self.on_trait_change( self.t1_trait, 't1' )
-        self.on_trait_change( self.t2_trait, 't2' )
-        self.on_trait_change( self.on_anytrait )
-        self.defer_trait_change()
-        self.t1 = -3
-        self.t2 = -4
-        self.defer_trait_change( False )
-        self.on_trait_change( self.t1_trait, 't1', remove = True )
-        self.on_trait_change( self.t2_trait, 't2', remove = True )
-        self.on_trait_change( self.on_anytrait,    remove = True )
-        print 'enabling trait changes in __init__'
-        self.defer_trait_change( False )
-        
-    def on_anytrait ( self, object, name, old, new ):
-        print 'on anytrait changed:', name, old, new
-        
-    def t1_trait ( self, object, name, old, new ):
-        print 'on t1 changed:', old, new
-        
-    def t2_trait ( self, object, name, old, new ):
-        print 'on t2 changed:', old, new
-        
-#------------------------------------------------------------------------------
-
 test1 = Test1()
 test1.test()
 
@@ -249,11 +151,3 @@ test7.test()
 
 test8 = Test8()
 test8.test()
-
-if ( HAVE_DEFER_TRAIT_CHANGE ) :
-    test9 = Test9()
-    test9.test()
-
-    test10 = Test10()
-    test10.test()
-
