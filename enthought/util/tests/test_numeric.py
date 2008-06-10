9179b34c72700859913039253775e9c2db7d8879
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-10 22:35:25 +0000

    Fixed tests and cleanup

--- enthought/util/tests/test_numeric.py
+++ enthought/util/tests/test_numeric.py
@@ -11,37 +11,37 @@
 # Author: Enthought, Inc.
 # Description: <Enthought util package component>
 #------------------------------------------------------------------------------
+import unittest
+
 from scipy import *
 
 from enthought.util.numeric import *
-import unittest
-#from scipy_test.testing import assert_almost_equal, assert_array_almost_equal
 
 
 class test_numeric(unittest.TestCase):
     
-    def check_safe_take(self):
+    def test_safe_take(self):
         a = array((1,2,3,4,2,2,7))
         b = array((2,2,2))
         indices = array((1,4,5))
         x = safe_take(a,indices)
-        self.assert_(x == b)
+        self.assert_(all(x == b))
     
         a = 2
         indices = array((0))
         x = safe_take(a,indices)
         self.assert_(x == a)
     
-    def check_safe_copy(self):
+    def test_safe_copy(self):
         a = arange(1,4,1)
         x = safe_copy(a)
-        self.assert_(x == a)
-    
+        self.assert_(all(x == a))
+        
         a = 4.1
         x = safe_copy(a)
         self.assert_(x == a)
     
-    def check_safe_min(self):
+    def test_safe_min(self):
         a = array((1,2,3,4,5,-6,-7))
         x = safe_min(a)
         self.assert_(x == -7)
@@ -50,7 +50,7 @@ class test_numeric(unittest.TestCase):
         x = safe_min(a)
         self.assert_(x == 5.)
     
-    def check_safe_max(self):
+    def test_safe_max(self):
         a = array((1,2,3,4,5,-6,-7))
         x = safe_max(a)
         self.assert_(x == 5)
@@ -59,7 +59,7 @@ class test_numeric(unittest.TestCase):
         x = safe_max(a)
         self.assert_(x == 5.)
     
-    def check_safe_len(self):
+    def test_safe_len(self):
         a = array((1,2,3,4,5,-6,-7))
         x = safe_len(a)
         self.assert_(x == 7)
@@ -69,22 +69,5 @@ class test_numeric(unittest.TestCase):
         self.assert_(x == 1)
             
     
-def test_suite(level=1):
-    suites = []
-    if level > 0:
-        suites.append( unittest.makeSuite(test_math,'check_') )
-    if level > 5:
-        pass
-    total_suite = unittest.TestSuite(suites)
-    return total_suite
-
-def test(level=10,verbosity=1):
-    all_tests = test_suite(level=level)
-    runner = unittest.TextTestRunner(verbosity=verbosity)
-    runner.run(all_tests)
-    return runner
-
 if __name__ == "__main__":
-    test()       
-    
-    
+    unittest.main()
