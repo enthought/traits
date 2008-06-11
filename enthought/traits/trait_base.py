274f302010a9dbdf91fcefd49e3d035ebde2b804
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-11 22:12:24 +0000

    Added an 'xgetattr' utility function to trait_base.py to simplify getting extended object attribute values.

--- enthought/traits/trait_base.py
+++ enthought/traits/trait_base.py
@@ -449,3 +449,26 @@ def get_resource_path ( level = 2 ):
 
     return path
 
+#-------------------------------------------------------------------------------
+#  Returns the value of an extended object attribute name of the form: 
+#      name[.name2[.name3...]]:
+#-------------------------------------------------------------------------------
+
+def xgetattr( object, xname, default = Undefined ):
+    """ Returns the value of an extended object attribute name of the form: 
+            name[.name2[.name3...]].
+    """
+    names = xname.split( '.' )
+    for name in names[:-1]:
+        if default is Undefined:
+            object = getattr( object, name )
+        else:
+            object = getattr( object, name, None )
+            if object is None:
+                return default
+                
+    if default is Undefined:
+        return getattr( object, names[-1] )
+        
+    return getattr( object, names[-1], default )
+        
