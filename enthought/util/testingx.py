52bccfdecf536f7d6b34f6575270bd803178ad8f
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-11 21:31:54 +0000

    Deprecated

--- enthought/util/testingx.py
+++
@@ -1,9 +0,0 @@
-"""
-testingx imports either Numeric or numpy based
-testing modules. See numerix for more information on selectors.
-"""
-from numerix import which
-if which[0]=='numpy':
-    from numpy.testing import *
-else:
-    from scipy_test.testing import *
