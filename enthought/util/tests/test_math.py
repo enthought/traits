983efca0da8abef90cb4ccaa376b0b83d7398225
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-11 15:08:26 +0000

    Fixed tests and clenaup

--- enthought/util/tests/test_math.py
+++ enthought/util/tests/test_math.py
@@ -11,45 +11,26 @@
 # Author: Enthought, Inc.
 # Description: <Enthought util package component>
 #------------------------------------------------------------------------------
-from scipy import *
-
-from enthought.util.math import *
 import unittest
-#from scipy_test.testing import assert_almost_equal, assert_array_almost_equal
+
+import enthought.util.math as M
 
 
 class test_math(unittest.TestCase):
     
-    def check_is_monotonic(self):
-        a = array((1,2,3,4,5,6,7))
-        self.assert_(is_monotonic(a) == True)
-        a = array((1,2,3,-1000,5,6,7))
-        self.assert_(is_monotonic(a) == False)
-        a = array((1))
-        self.assert_(is_monotonic(a) == False)
+    def test_is_monotonic(self):
+        a = M.array((1,2,3,4,5,6,7))
+        self.assert_(M.is_monotonic(a) == True)
+        a = M.array((1,2,3,-1000,5,6,7))
+        self.assert_(M.is_monotonic(a) == False)
+        a = M.array((1))
+        self.assert_(M.is_monotonic(a) == False)
     
-    def check_brange(self):
-        a = arange(1,5,1)
-        b = brange(1,4,1)
-        self.assert_(allclose(a, b))
+    def test_brange(self):
+        a = M.arange(1,5,1)
+        b = M.brange(1,4,1)
+        self.assert_(M.allclose(a, b))
         
     
-def test_suite(level=1):
-    suites = []
-    if level > 0:
-        suites.append( unittest.makeSuite(test_math,'check_') )
-    if level > 5:
-        pass
-    total_suite = unittest.TestSuite(suites)
-    return total_suite
-
-def test(level=10,verbosity=1):
-    all_tests = test_suite(level=level)
-    runner = unittest.TextTestRunner(verbosity=verbosity)
-    runner.run(all_tests)
-    return runner
-
 if __name__ == "__main__":
-    test()       
-    
-    
+    unittest.main()
