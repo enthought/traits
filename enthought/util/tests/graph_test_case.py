f510cab2dbe4acb64f4482b98c8510568bef89ca
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-11 15:24:40 +0000

    Fixed and cleaned

--- enthought/util/tests/graph_test_case.py
+++ enthought/util/tests/graph_test_case.py
@@ -2,18 +2,12 @@ import sys, unittest
 
 from enthought.testing.api import doctest_for_module
 
-import enthought.util.graph as graph
-from enthought.util.graph import *
+import enthought.util.graph as G
 
-class GraphDocTestCase(doctest_for_module(graph)):
+
+class GraphDocTestCase(doctest_for_module(G)):
     pass
 
-class MapTestCase(unittest.TestCase):
-    def test_map(self):
-        'map'
-        self.assertEqual(map(str, {}), {})
-        self.assertEqual(map(str, {1:[2,3]}), {'1':['2','3']})
-        self.assertEqual(map(lambda x: x, {1:[2,3]}), {1:[2,3]})
 
 class ReachableGraphTestCase(unittest.TestCase):
 
@@ -21,7 +15,7 @@ class ReachableGraphTestCase(unittest.TestCase):
         if error:
             self.assertRaises(error, lambda: self._base(graph, nodes, result))
         else:
-            self.assertEqual(reachable_graph(graph, nodes), result)
+            self.assertEqual(G.reachable_graph(graph, nodes), result)
 
     def test_reachable_graph(self):
         'reachable_graph'
@@ -34,12 +28,13 @@ class ReachableGraphTestCase(unittest.TestCase):
         self._base({1:[2,3], 2:[3]}, [3], {})
         self._base({1:[2], 3:[4]}, [1,3], {1:[2], 3:[4]})
 
+
 class ReverseGraphTestCase(unittest.TestCase):
     def _base(self, graph, result, error=None):
         if error:
             self.assertRaises(error, lambda: self._base(graph, result))
         else:
-            self.assertEqual(reverse(graph), result)
+            self.assertEqual(G.reverse(graph), result)
 
     def test_reverse(self):
         'reverse'
@@ -47,7 +42,6 @@ class ReverseGraphTestCase(unittest.TestCase):
         self._base({1:[]}, {1:[]})
         self._base({1:[2]}, {2:[1], 1:[]})
         self._base({1:[2,3]}, {2:[1], 3:[1], 1:[]})
-
 
 
 if __name__ == '__main__':
