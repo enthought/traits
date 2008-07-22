1fe438c5bff4972b74abfc8f625eab2f7ea56674
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-22 03:37:43 +0000

    Adding docs/source with copy of current docs, html.zip with zipped html doc, make_docs.py, and html generation code to setup.py.

---
+++ docs/source/index.rst
@@ -0,0 +1,13 @@
+.. EnthoughtBase documentation master file, created by sphinx-quickstart on Mon Jul 21 22:26:13 2008.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to EnthoughtBase's documentation!
+=========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   toolkit_selection.rst
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os, zipfile
+
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from distutils.command.build import build as distbuild
+from distutils import log
+from pkg_resources import require, DistributionNotFound
+
+from make_docs import HtmlBuild
 
 
 # NOTE: EnthoughtBase should *never* depend on another ETS project!  We need
@@ -31,9 +39,84 @@ TRAITS_UI = etsdep('Traits[ui]', '3.0.0b1')
 #APPTOOLS -- util/wx/drag_and_drop
 #SCIMATH -- util/wx/spreadsheet/unit_renderer.py
 
+def generate_docs():
+    """If sphinx is installed, generate docs.
+    """
+    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                           'source')
+    html_zip = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                            'html.zip')
+    dest_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            'docs')
+    
+    try:
+        require("Sphinx>=0.4.1")
+            
+        log.info("Auto-generating documentation in %s/html" % dest_dir)
+        doc_src = doc_dir
+        target = dest_dir
+        try:
+            build = HtmlBuild()
+            build.start({
+                'commit_message': None,
+                'doc_source': doc_src,
+                'preserve_temp': True,
+                'subversion': False,
+                'target': target,
+                'verbose': True,
+                'versioned': False,
+                }, [])
+            del build
+        except:
+            log.error("The documentation generation failed."
+                      " Installing from zip file.")
+            
+            # Unzip the docs into the 'html' folder.
+            unzip_html_docs(html_zip, dest_dir)
+            
+    except DistributionNotFound:
+        log.error("Sphinx is not installed, so the documentation could not be "
+                  "generated.  Installing from zip file...")
+        
+        # Unzip the docs into the 'html' folder.
+        unzip_html_docs(html_zip, dest_dir)
+
+def unzip_html_docs(src_path, dest_dir):
+    """Given a path to a zipfile, extract
+    its contents to a given 'dest_dir'.
+    """
+    file = zipfile.ZipFile(src_path)
+    for name in file.namelist():
+        cur_name = os.path.join(dest_dir, name)
+        if not name.endswith('/'):
+            out = open(cur_name, 'wb')
+            out.write(file.read(name))
+            out.flush()
+            out.close()
+        else:
+            if not os.path.exists(cur_name):
+                os.mkdir(cur_name)
+    file.close()
+
+class my_develop(develop):
+    def run(self):
+        develop.run(self)
+        # Generate the documentation.
+        generate_docs()
+
+class my_build(distbuild):
+    def run(self):
+        distbuild.run(self)
+        # Generate the documentation.
+        generate_docs()
+
 setup(
     author = 'Enthought, Inc',
     author_email = 'info@enthought.com',
+    cmdclass = {
+        'develop': my_develop,
+        'build': my_build
+    },
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
         ],
