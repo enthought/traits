74687096c2287783caf9dd152fd9e8b344d2f543
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-23 19:18:29 +0000

    Corrected what looks to have been some kind of a transription error in the example code.

--- examples/tutorials/doc_examples/examples/mapped.py
+++ examples/tutorials/doc_examples/examples/mapped.py
@@ -4,29 +4,30 @@
 from enthought.traits.api import HasTraits, Trait
 
 #--[Code]-----------------------------------------------------------------------
-standard_color = Trait ('black', 
-              {'black':       (0.0, 0.0, 0.0, 1.0),
-               'blue':        (0.0, 0.0, 1.0, 1.0),
-               'cyan':        (0.0, 1.0, 1.0, 1.0),
-               'green':       (0.0, 1.0, 0.0, 1.0),
-               'magenta':     (1.0, 0.0, 1.0, 1.0),
-               'orange':      (0.8, 0.196, 0.196, 1.0),
-               'purple':      (0.69, 0.0, 1.0, 1.0),
-               'red':         (1.0, 0.0, 0.0, 1.0),
-               'violet':      (0.31, 0.184, 0.31, 1.0),
-               'yellow':      (1.0, 1.0, 0.0, 1.0),
-               'white':       (1.0, 1.0, 1.0, 1.0),
-               'transparent': (1.0, 1.0, 1.0, 0.0) } )
+standard_color = Trait ('black', {
+    'black':       ( 0.0, 0.0, 0.0, 1.0 ),
+    'blue':        ( 0.0, 0.0, 1.0, 1.0 ),
+    'cyan':        ( 0.0, 1.0, 1.0, 1.0 ),
+    'green':       ( 0.0, 1.0, 0.0, 1.0 ),
+    'magenta':     ( 1.0, 0.0, 1.0, 1.0 ),
+    'orange':      ( 0.8, 0.196, 0.196, 1.0 ),
+    'purple':      ( 0.69, 0.0, 1.0, 1.0 ),
+    'red':         ( 1.0, 0.0, 0.0, 1.0 ),
+    'violet':      ( 0.31, 0.184, 0.31, 1.0 ),
+    'yellow':      ( 1.0, 1.0, 0.0, 1.0 ),
+    'white':       ( 1.0, 1.0, 1.0, 1.0 ),
+    'transparent': ( 1.0, 1.0, 1.0, 0.0 ) 
+} )
                
-red_color = Trait ('red', standard_color)
+red_color = Trait( 'red', standard_color )
 
-class GraphicShape (HasTraits): 
+class GraphicShape( HasTraits ): 
     line_color = standard_color
     fill_color = red_color
     
 #--[Example*]-------------------------------------------------------------------
     
-my_shape1 = mapped.GraphicShape()
+my_shape1 = GraphicShape()
 
 # Default values for normal trait attributes
 print my_shape1.line_color, my_shape1.fill_color
@@ -37,12 +38,13 @@ print my_shape1.line_color_, my_shape1.fill_color_
 # Output: (0.0, 0.0, 0.0, 1.0) (1.0, 0.0, 0.0, 1.0)
 
 # Non-default values
-my_shape2 = mapped.GraphicShape()
+my_shape2 = GraphicShape()
 my_shape2.line_color = 'blue'
 my_shape2.fill_color = 'green'
 
 print my_shape2.line_color, my_shape2.fill_color
 # Output: blue green
+
 print my_shape2.line_color_, my_shape2.fill_color_
 # Output: (0.0, 0.0, 1.0, 1.0) (0.0, 1.0, 0.0, 1.0)
     
