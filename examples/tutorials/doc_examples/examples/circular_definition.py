9105f3809879f2f0307930e6796626026d17d150
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-23 19:35:11 +0000

    Modified the example to print the exception message rather than the entire traceback, thus making it look like an expected result, rather than a bug.

--- examples/tutorials/doc_examples/examples/circular_definition.py
+++ examples/tutorials/doc_examples/examples/circular_definition.py
@@ -1,19 +1,21 @@
 #--[Imports]--------------------------------------------------------------------
 
-from enthought.traits.api import HasTraits, Trait
+from enthought.traits.api import HasTraits, Trait, Instance
 
 #--[Code]-----------------------------------------------------------------------
 
 # Shows the incorrect way of defining mutually-referring classes.
+try:   
+    class Chicken ( HasTraits ):
+    
+        # Won't work: 'Egg' not defined yet:
+        hatched_from = Instance( Egg )
+        
+    class Egg ( HasTraits ):
+    
+        # If we move this class to the top, then this line won't work, because
+        # 'Chicken' won't be defined yet:
+        created_by = Instance( Chicken )
+except NameError, excp:
+    print excp
 
-class Chicken ( HasTraits ):
-    
-    # Won't work: 'Egg' not defined yet:
-    hatched_from = Instance( Egg )
-
-    
-class Egg ( HasTraits ):
-    
-    # If we move this class to the top, then this line won't work, because
-    # 'Chicken' won't be defined yet:
-    created_by = Instance( Chicken )
