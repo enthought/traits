0fba5d77258f2709fff7c275a4813757e77fd1bb
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-23 15:32:02 +0000

    Modified setup.py with html gen code and to use setup_data.py for version info.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+import os, zipfile
 from setuptools import setup, Extension, find_packages
+from setuptools.command.develop import develop
+from distutils.command.build import build as distbuild
+from distutils import log
+from pkg_resources import require, DistributionNotFound
 
+from setup_data import INFO
+from make_docs import HtmlBuild
 
 ctraits = Extension(
     'enthought.traits.ctraits',
@@ -47,10 +54,84 @@ TRAITSGUI       = etsdep( 'TraitsGUI',       '3.0.0b1' )
 #   practice it should not cause any  problems. Leaving the dependency in
 #   pulls the TraitsBackendWX egg in, even if it is not needed.
 
+def generate_docs():
+    """If sphinx is installed, generate docs.
+    """
+    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                           'source')
+    html_zip = os.path.join(os.path.abspath(os.path.dirname(__file__)),'docs',
+                            'html.zip')
+    dest_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            'docs')
+    
+    try:
+        require("Sphinx>=0.4.1")
+            
+        log.info("Auto-generating documentation in %s/html" % dest_dir)
+        doc_src = doc_dir
+        target = dest_dir
+        try:
+            build = HtmlBuild()
+            build.start({
+                'commit_message': None,
+                'doc_source': doc_src,
+                'preserve_temp': True,
+                'subversion': False,
+                'target': target,
+                'verbose': True,
+                'versioned': False,
+                }, [])
+            del build
+        except:
+            log.error("The documentation generation failed."
+                      " Installing from zip file.")
+            
+            # Unzip the docs into the 'html' folder.
+            unzip_html_docs(html_zip, dest_dir)
+            
+    except DistributionNotFound:
+        log.error("Sphinx is not installed, so the documentation could not be "
+                  "generated.  Installing from zip file...")
+        
+        # Unzip the docs into the 'html' folder.
+        unzip_html_docs(html_zip, dest_dir)
+
+def unzip_html_docs(src_path, dest_dir):
+    """Given a path to a zipfile, extract
+    its contents to a given 'dest_dir'.
+    """
+    file = zipfile.ZipFile(src_path)
+    for name in file.namelist():
+        cur_name = os.path.join(dest_dir, name)
+        if not name.endswith('/'):
+            out = open(cur_name, 'wb')
+            out.write(file.read(name))
+            out.flush()
+            out.close()
+        else:
+            if not os.path.exists(cur_name):
+                os.mkdir(cur_name)
+    file.close()
+
+class my_develop(develop):
+    def run(self):
+        develop.run(self)
+        # Generate the documentation.
+        generate_docs()
+
+class my_build(distbuild):
+    def run(self):
+        distbuild.run(self)
+        # Generate the documentation.
+        generate_docs()
 
 setup(
     author       = 'David C. Morrill',
     author_email = 'dmorrill@enthought.com',
+    cmdclass = {
+        'develop': my_develop,
+        'build': my_build
+    },
     description  = 'Explicitly typed Python attributes package',
 
     extras_require = {
@@ -121,7 +202,7 @@ setup(
     ],
     test_suite = 'nose.collector',
     url        = 'http://code.enthought.com/traits',
-    version    = '3.0.0b1',
+    version    = INFO['version'],
     zip_safe   = False,
 )
 
