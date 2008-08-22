c723b4f3cb1962409ce470e51c20a36ee541d7b2
vibha <vibha@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-22 18:13:54 +0000

    Modify the editors_gen file so it looks at a definition file to get the editor (or editor factory) class name to be used (instead of going through all the editors declared in the various backends).

--- enthought/traits/ui/editors_gen.py
+++ enthought/traits/ui/editors_gen.py
@@ -8,51 +8,51 @@
 # Author: Vibha Srinivasan <vibha@enthought.com>
 #------------------------------------------------------------------------------
 
-""" Generates a file containing definitions for the editors defined in the 
-various backends.
+""" Generates a file containing definitions for editors defined in the various
+backends.
 """
 
 import os, glob, sys
-from enthought.traits.ui.editor import Editor
-from enthought.traits.ui.editor_factory import EditorFactory
-from toolkit import TraitUIToolkits
 
-def gen_editor_definitions(target_filename):
-    """ Generates a file containing definitions for the editors defined in the 
-    various backends.
+def gen_editor_definitions(target_filename = 'editors.py'):
+    """ Generates a file containing definitions for editors defined in the 
+       various backends.
+
+    The idea is that if a new editor has been declared in any of the backends,
+    the author needs to create a file called '***_definition' in the Traits
+    package (in enthought.traits.ui). This function will be run each time the
+    user runs the setup.py file, and the new editor's definition will be 
+    appended to the editors.py file.
+ 
+    The structure of the ***_definition file should be as follows:
+    ***_definition = '<file name in the backend package>:
+                     <name of the Editor or the EditorFactory class'
     """
 
     base_import_path = 'enthought.traits.ui.'
-    editor_generators = []
-    for toolkit in TraitUIToolkits:
-        import_path = base_import_path + toolkit + '.editors'
-        try:
+    definition_files = glob.glob('*_definition.py')
+    new_editors = []
+    for file in definition_files:
+           import_path = base_import_path + file.rstrip('.py')
            __import__(import_path)
            mod = sys.modules[import_path]
-           editor_generators.extend([
-              (name, ''.join([item.capitalize() for item in name.split('_')])) 
-              for name in dir(mod) if '_editor' in name])
-        except Exception, e:
-           pass   
-    editors = dict(zip(editor_generators, range(len(editor_generators)))).keys()
-    target_file = open(target_filename, 'w')
-    prelims = \
-"""
-import sys
-from toolkit import toolkit
-
-"""
-    target_file.write(prelims)
-    for name, class_name in editors:
-            function = "def %s(*args, **traits):"%class_name
+           for name in dir(mod):
+               if '_definition' in name:
+                   (file_name, class_name) = getattr(mod, name).split(':')
+                   new_editors.append((file_name, class_name))
+    
+    target_file = open(target_filename, 'a')
+    for editor_name in new_editors:
+            function = "def %s( *args, **traits ):\n" % class_name
             target_file.write(function)
-            target_file.write('\n')
-            func_code = \
-    """
-    name = toolkit().__module__.rstrip('.toolkit') + '.editors'
-    __import__(name)
-    """
-            target_file.write(func_code)
-            target_file.write("return sys.modules[name].%s( *args, **traits )" % name)
+            func_code =  ' '*4 + 'import sys\n'
+            func_code += ' '*4 + \
+                         'name = toolkit().__module__.rstrip(".toolkit")' + \
+                         ' + ".%s"\n' % file_name 
+            func_code += ' '*4 + "__import__(name)\n"
+            func_code += ' '*4 + \
+                         'return sys.modules[name].%s( *args, **traits )' \
+                         % class_name
+            target_file.write(func_code) 
             target_file.write('\n\n')  
     target_file.close()   
