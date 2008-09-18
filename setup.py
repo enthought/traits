e8f097bfbe1ed9be4f1486586739539003b3d205
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-18 04:35:42 +0000

    SETUP: Removed unused imports, code, and file. Added support for new setupdocs package. Added alias so docs will get built whenever bdist_egg is run. Minor formatting issues.

--- setup.py
+++ setup.py
@@ -56,9 +56,6 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.develop import develop
 from distutils.command.build import build as distbuild
 from distutils import log
-from pkg_resources import DistributionNotFound, parse_version, require, VersionConflict
-
-from make_docs import HtmlBuild
 
 # FIXME: This works around a setuptools bug which gets setup_data.py metadata
 # from incorrect packages. Ticket #1592
@@ -80,81 +77,8 @@ speedups = Extension(
     extra_compile_args = ['-DNDEBUG=1', '-O3'],
     )
 
-
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
-
-
-# Methods to allow us to dynamically build html docs from RST sources.
-def generate_docs():
-    """ If sphinx is installed, generate docs.
-    """
-    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs')
-    source_dir = os.path.join(doc_dir, 'source')
-    html_zip = os.path.join(doc_dir,  'html.zip')
-    dest_dir = doc_dir
-
-    required_sphinx_version = "0.4.1"
-    sphinx_installed = False
-    try:
-        require("Sphinx>=%s" % required_sphinx_version)
-        sphinx_installed = True
-    except (DistributionNotFound, VersionConflict):
-        log.warn('Sphinx install of version %s could not be verified.'
-                    ' Trying simple import...' % required_sphinx_version)
-        try:
-            import sphinx
-            if parse_version(sphinx.__version__) < parse_version(required_sphinx_version):
-                log.error("Sphinx version must be >=%s." % required_sphinx_version)
-            else:
-                sphinx_installed = True
-        except ImportError:
-            log.error("Sphinx install not found.")
-
-    if sphinx_installed:
-        log.info("Generating %s documentation..." % INFO['name'])
-        docsrc = source_dir
-        target = dest_dir
-
-        try:
-            build = HtmlBuild()
-            build.start({
-                'commit_message': None,
-                'doc_source': docsrc,
-                'preserve_temp': True,
-                'subversion': False,
-                'target': target,
-                'verbose': True,
-                'versioned': False
-                }, [])
-            del build
-
-        except:
-            log.error("The documentation generation failed.  Falling back to "
-                      "the zip file.")
-
-            # Unzip the docs into the 'html' folder.
-            unzip_html_docs(html_zip, doc_dir)
-    else:
-        # Unzip the docs into the 'html' folder.
-        log.info("Installing %s documentation from zip file.\n" % INFO['name'])
-        unzip_html_docs(html_zip, doc_dir)
-
-def unzip_html_docs(src_path, dest_dir):
-    """ Given a path to a zipfile, extract its contents to a given 'dest_dir'.
-    """
-    file = zipfile.ZipFile(src_path)
-    for name in file.namelist():
-        cur_name = os.path.join(dest_dir, name)
-        if not name.endswith('/'):
-            out = open(cur_name, 'wb')
-            out.write(file.read(name))
-            out.flush()
-            out.close()
-        else:
-            if not os.path.exists(cur_name):
-                os.mkdir(cur_name)
-    file.close()
 
 def gen_editors():
     from enthought.traits.ui import editors_gen
@@ -168,17 +92,20 @@ def gen_editors():
     print "Done."
     print '-'*70
 
-class my_develop(develop):
+
+class MyDevelop(develop):
     def run(self):
         gen_editors()
         develop.run(self)
-        generate_docs()
+        self.run_command('build_docs')
 
-class my_build(distbuild):
+
+class MyBuild(distbuild):
     def run(self):
         gen_editors()
         distbuild.run(self)
-        generate_docs()
+        self.run_command('build_docs')
+
 
 # Call the setup function.
 setup(
@@ -201,8 +128,8 @@ setup(
         Topic :: Software Development :: Libraries
         """.splitlines() if len(c.strip()) > 0],
     cmdclass = {
-        'develop': my_develop,
-        'build': my_build
+        'develop': MyDevelop,
+        'build': MyBuild
         },
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
@@ -229,6 +156,7 @@ setup(
         'integrationtests.*',
         ]),
     platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    setup_requires = 'setupdocs',
     tests_require = [
         'nose >= 0.10.3',
         ],
