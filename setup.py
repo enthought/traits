209babc54b2b71537ff9d45dd8b13a44f7b4dfc3
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-18 04:15:29 +0000

    SETUP: Removed unused imports, code, and file. Added support for new setupdocs package. Added alias so docs will get built whenever bdist_egg is run. Minor formatting issues.

--- setup.py
+++ setup.py
@@ -30,9 +30,6 @@ If you want to build EnthoughtBase from source, you must first install
 
 from distutils import log
 from distutils.command.build import build as distbuild
-from make_docs import HtmlBuild
-from pkg_resources import DistributionNotFound, parse_version, require, \
-    VersionConflict
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 import os
@@ -49,88 +46,15 @@ INFO = setup_data['INFO']
 DOCLINES = __doc__.split("\n")
 
 
-# Functions to build docs from source when building this project.
-def generate_docs():
-    """ If sphinx is installed, generate docs.
-    """
-    doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs')
-    source_dir = os.path.join(doc_dir, 'source')
-    html_zip = os.path.join(doc_dir,  'html.zip')
-    dest_dir = doc_dir
-
-    required_sphinx_version = "0.4.1"
-    sphinx_installed = False
-    try:
-        require("Sphinx>=%s" % required_sphinx_version)
-        sphinx_installed = True
-    except (DistributionNotFound, VersionConflict):
-        log.warn('Sphinx install of version %s could not be verified.'
-            ' Trying simple import...' % required_sphinx_version)
-        try:
-            import sphinx
-            if parse_version(sphinx.__version__) < parse_version(
-                required_sphinx_version):
-                log.error("Sphinx version must be >=" + \
-                    "%s." % required_sphinx_version)
-            else:
-                sphinx_installed = True
-        except ImportError:
-            log.error("Sphinx install not found.")
-
-    if sphinx_installed:
-        log.info("Generating %s documentation..." % INFO['name'])
-        docsrc = source_dir
-        target = dest_dir
-
-        try:
-            build = HtmlBuild()
-            build.start({
-                'commit_message': None,
-                'doc_source': docsrc,
-                'preserve_temp': True,
-                'subversion': False,
-                'target': target,
-                'verbose': True,
-                'versioned': False
-                }, [])
-            del build
-
-        except:
-            log.error("The documentation generation failed.  Falling back to "
-                "the zip file.")
-
-            # Unzip the docs into the 'html' folder.
-            unzip_html_docs(html_zip, doc_dir)
-    else:
-        # Unzip the docs into the 'html' folder.
-        log.info("Installing %s documentation from zip file.\n" % INFO['name'])
-        unzip_html_docs(html_zip, doc_dir)
-
-def unzip_html_docs(src_path, dest_dir):
-    """ Given a path to a zipfile, extract its contents to a given 'dest_dir'.
-    """
-    file = zipfile.ZipFile(src_path)
-    for name in file.namelist():
-        cur_name = os.path.join(dest_dir, name)
-        if not name.endswith('/'):
-            out = open(cur_name, 'wb')
-            out.write(file.read(name))
-            out.flush()
-            out.close()
-        else:
-            if not os.path.exists(cur_name):
-                os.mkdir(cur_name)
-    file.close()
-
-class my_develop(develop):
+class MyDevelop(develop):
     def run(self):
         develop.run(self)
-        generate_docs()
+        self.run_command('build_docs')
 
-class my_build(distbuild):
+class MyBuild(distbuild):
     def run(self):
         distbuild.run(self)
-        generate_docs()
+        self.run_command('build_docs')
 
 
 # The actual setup call.
@@ -153,8 +77,8 @@ setup(
         Topic :: Software Development :: Libraries
         """.splitlines() if len(c.strip()) > 0],
     cmdclass = {
-        'develop': my_develop,
-        'build': my_build
+        'develop': MyDevelop,
+        'build': MyBuild
     },
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
@@ -173,6 +97,7 @@ setup(
         ],
     packages = find_packages(),
     platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    setup_requires = 'setupdocs',
     tests_require = [
         'nose >= 0.10.3',
         ],
