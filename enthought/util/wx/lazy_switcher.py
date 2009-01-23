e4eeef9011c7d8979468e04e5460d25bad0d9b1b
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-01-23 19:16:59 +0000

    Fixing import.

--- enthought/util/wx/lazy_switcher.py
+++ enthought/util/wx/lazy_switcher.py
@@ -16,7 +16,7 @@
 
 # Major package imports.
 import wx
-from wx.lib.scrolledpanel import wxScrolledPanel
+from wx.lib.scrolledpanel import ScrolledPanel as wxScrolledPanel
 
 # Enthought library imports.
 from enthought.traits.api import HasTraits, Int
