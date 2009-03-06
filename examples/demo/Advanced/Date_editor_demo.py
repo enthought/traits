745b824787869b7058a238a70aaa8ef33565b6fe
judah <judah@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-06 20:04:15 +0000

    Updated the Date editor demo to include a 'readonly' style item too.

--- examples/demo/Advanced/Date_editor_demo.py
+++ examples/demo/Advanced/Date_editor_demo.py
@@ -1,22 +1,67 @@
-#  Copyright (c) 2007, Enthought, Inc.
+#  Copyright (c) 2007-2009, Enthought, Inc.
 #  License: BSD Style.
 
 """
 A Traits UI editor that wraps a WX calendar panel.
 """
 
-from enthought.traits.api import HasTraits, Date
-from enthought.traits.ui.api import View, Item
+from enthought.traits.api import HasTraits, Date, List, Str, on_trait_change
+from enthought.traits.ui.api import View, Item, DateEditor, Group
 
 
 class DateEditorDemo(HasTraits):
-    """ Demo class. """
-    date = Date
-    view = View(Item('date', show_label=False), resizable=True)
+    """ Demo class to show Date editors. """
+    simple_date = Date
+    single_date = Date()
+    multi_date = List(Date)
+    info_string = Str('The editors for Traits Date objects.  Showing both '\
+                      'the defaults, and one with alternate options.')
+    
+    multi_select_editor = DateEditor(multi_select=True,
+                                     months=2,
+                                     allow_future=False,
+                                     padding=30,
+                                     on_mixed_select='max_change',
+                                     shift_to_select=False)
+    
+    view = View(Item('info_string', 
+                     show_label=False,
+                     style='readonly'),
+                
+                Group(Item('simple_date', 
+                           label='Simple date editor'),
+                      Item('single_date', 
+                           style='custom',
+                           label='Default custom editor'),
+                      Item('single_date',
+                           style='readonly',
+                           editor=DateEditor(strftime='You picked %B %d %Y',
+                                             message='Click a date above.'),
+                           label='ReadOnly editor'),
+                      label='Default settings for editors'),
+                
+                Group(Item('multi_date', 
+                           editor=multi_select_editor,
+                           style='custom',
+                           label='Multi-select custom editor'),
+                      label='More customized editor: multi-select; disallow '\
+                            'future; two months; padding; selection '\
+                            'style; etc.'), 
+                
+                resizable=True)
 
-    def _date_changed(self):
+
+    def _multi_date_items_changed(self):
         """ Print each time the date value is changed in the editor. """
-        print self.date
+        print self.multi_date
+        
+    def _simple_date_changed(self):
+        """ Print each time the date value is changed in the editor. """
+        print self.simple_date, self.single_date
+        
+    def _single_date_changed(self):
+        """ Print each time the date value is changed in the editor. """
+        print self.single_date
 
 
 #-- Set Up The Demo ------------------------------------------------------------
@@ -25,6 +70,5 @@ demo = DateEditorDemo()
 
 if __name__ == "__main__":
     demo.configure_traits()        
-    
 
 #-- eof ----------------------------------------------------------------------- 
