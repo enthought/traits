7f21882664f4d523c935e53bf54cb154d65c78ae
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 22:19:32 +0000

    * Smallest possible version number bump after making a release. * Updated dependency versions to match what is currently (or will soon be, after I finish checking everything in) in the trunk of other projects.

--- setup_data.py
+++ setup_data.py
@@ -20,10 +20,10 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.2')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.0')
-TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.1.0')
-TRAITSGUI = etsdep('TraitsGUI', '3.0.4')
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.3')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.1')
+TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.1.1')
+TRAITSGUI = etsdep('TraitsGUI', '3.0.5')
 # Notes:
 # - enthought\traits\ui\handler.py and
 #   enthought\traits\ui\dockable_view_element.py depend upon files in
@@ -77,6 +77,6 @@ INFO = {
     'install_requires': [
         ],
     'name': 'Traits',
-    'version': '3.1.0',
+    'version': '3.1.1',
     }
 
