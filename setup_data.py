6f5fcb38b89ba37a8f4c97fbe48f61602da8f491
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 22:19:21 +0000

    * Smallest possible version number bump after making a release. * Updated dependency versions to match what is currently (or will soon be, after I finish checking everything in) in the trunk of other projects.

--- setup_data.py
+++ setup_data.py
@@ -17,12 +17,12 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-ENVISAGECORE = etsdep('EnvisageCore', '3.1.0')  # -- all from logger.(plugin|agent|widget)
-ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.1.0')  # -- all from logger.plugin
-TRAITS = etsdep('Traits', '3.1.0')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.0')  # -- only from e.util.traits.editor.parameter_choice_editor.py
-TRAITSGUI = etsdep('TraitsGUI', '3.0.4')  # -- from logger.(agent|plugin|widget)
-TRAITS_UI = etsdep('Traits[ui]', '3.1.0')
+ENVISAGECORE = etsdep('EnvisageCore', '3.1.1')  # -- all from logger.(plugin|agent|widget)
+ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.1.1')  # -- all from logger.plugin
+TRAITS = etsdep('Traits', '3.1.1')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.1')  # -- only from e.util.traits.editor.parameter_choice_editor.py
+TRAITSGUI = etsdep('TraitsGUI', '3.0.5')  # -- from logger.(agent|plugin|widget)
+TRAITS_UI = etsdep('Traits[ui]', '3.1.1')
 
 # The following "soft dependencies" are wrapped in try..except blocks
 #APPTOOLS -- util/wx/drag_and_drop
@@ -63,6 +63,6 @@ INFO = {
     'install_requires' : [
         ],
     'name': 'EnthoughtBase',
-    'version': '3.0.2',
+    'version': '3.0.3',
     }
 
