6d2c575d56c2434c1c28002111bb02b462bf0961
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-31 21:06:11 +0000

    added editable_labels attribute to the TabularEditor for allowing editting of the 1st column if editable is False. Changed initialization of wxListCtrl to not call the TextEditMixin constructor if editable is False. These changes allow for 3 distinct modes of editing- strictly read-only, edittable first columns, or completely editable

--- enthought/traits/ui/editors/tabular_editor.py
+++ enthought/traits/ui/editors/tabular_editor.py
@@ -15,7 +15,7 @@
 #
 #-------------------------------------------------------------------------------
 
-""" A traits UI editor for editing tabular data (arrays, list of tuples, lists 
+""" A traits UI editor for editing tabular data (arrays, list of tuples, lists
     of objects, etc).
 """
 
@@ -25,16 +25,16 @@
 
 from enthought.traits.api \
     import Str, Bool, Property, List, Enum, Instance
-           
+
 from enthought.traits.ui.ui_traits \
     import Image
-    
+
 from enthought.traits.ui.basic_editor_factory \
     import BasicEditorFactory
 
 from enthought.traits.ui.toolkit \
     import toolkit_object
-    
+
 #-------------------------------------------------------------------------------
 #  'TabularEditor' editor factory class:
 #-------------------------------------------------------------------------------
@@ -42,88 +42,91 @@ from enthought.traits.ui.toolkit \
 class TabularEditor ( BasicEditorFactory ):
     """ Editor factory for tabular editors.
     """
-  
+
     #-- Trait Definitions ------------------------------------------------------
-    
+
     # The editor class to be created:
     klass = Property
-    
+
     # Should column headers (i.e. titles) be displayed?
     show_titles = Bool( True )
-    
+
     # The optional extended name of the trait used to indicate that a complete
     # table update is needed:
     update = Str
-    
-    # Should the table update automatically when the table item's contents 
+
+    # Should the table update automatically when the table item's contents
     # change? Note that in order for this feature to work correctly, the editor
-    # trait should be a list of objects derived from HasTraits. Also, 
+    # trait should be a list of objects derived from HasTraits. Also,
     # performance can be affected when very long lists are used, since enabling
     # this feature adds and removed Traits listeners to each item in the list.
     auto_update = Bool( False )
-    
-    # The optional extended name of the trait to synchronize the selection 
+
+    # The optional extended name of the trait to synchronize the selection
     # values with:
     selected = Str
-    
-    # The optional extended name of the trait to synchronize the selection rows 
+
+    # The optional extended name of the trait to synchronize the selection rows
     # with:
     selected_row = Str
-    
+
     # The optional extended name of the trait to synchronize the activated value
     # with:
     activated = Str
-    
-    # The optional extended name of the trait to synchronize the activated 
+
+    # The optional extended name of the trait to synchronize the activated
     # value's row with:
     activated_row = Str
-    
+
     # The optional extended name of the trait to synchronize left click data
     # with. The data is a TabularEditorEvent:
     clicked = Str
-    
+
     # The optional extended name of the trait to synchronize left double click
     # data with. The data is a TabularEditorEvent:
     dclicked = Str
-    
+
     # The optional extended name of the trait to synchronize right click data
     # with. The data is a TabularEditorEvent:
     right_clicked = Str
-    
+
     # The optional extended name of the trait to synchronize right double
     # clicked data with. The data is a TabularEditorEvent:
     right_dclicked = Str
-    
+
     # The optional extended name of the trait to synchronize column
     # clicked data with. The data is a TabularEditorEvent:
     column_clicked = Str
-    
+
     # Can the user edit the values?
     editable = Bool( True )
-                 
+
+    # Can the user edit the labels (i.e. the first column)
+    editable_labels = Bool( False )
+
     # Are multiple selected items allowed?
     multi_select = Bool( False )
-    
+
     # Should horizontal lines be drawn between items?
     horizontal_lines = Bool( True )
-    
+
     # Should vertical lines be drawn between items?
     vertical_lines = Bool( True )
-           
-    # The adapter from trait values to editor values:                       
+
+    # The adapter from trait values to editor values:
     adapter = Instance( 'enthought.traits.ui.tabular_adapter.TabularAdapter', () )
-    
+
     # What type of operations are allowed on the list:
     operations = List( Enum( 'delete', 'insert', 'append', 'edit', 'move' ),
                        [ 'delete', 'insert', 'append', 'edit', 'move' ] )
-                       
-    # Are 'drag_move' operations allowed (i.e. True), or should they always be 
+
+    # Are 'drag_move' operations allowed (i.e. True), or should they always be
     # treated as 'drag_copy' operations (i.e. False):
     drag_move = Bool( False )
-                           
-    # The set of images that can be used:                       
+
+    # The set of images that can be used:
     images = List( Image )
-    
+
     def _get_klass(self):
         """ Returns the toolkit-specific editor class to be instantiated.
         """
