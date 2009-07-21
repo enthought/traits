c8d6a6d2ce0ad9abcf6fc48ad375b044d4f6c18a
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-21 23:29:04 +0000

    * Updated all __init__.py files with modulefinder code so that py2app/py2exe can correctly determine what to include when packaging a distribution.  This is necessary due to the use of namespace packages.

--- enthought/traits/ui/__init__.py
+++ enthought/traits/ui/__init__.py
@@ -23,3 +23,11 @@ try:
     __import__('pkg_resources').declare_namespace(__name__)
 except:
     pass
+
+# For py2app / py2exe support
+try:
+    import modulefinder
+    for p in __path__:
+        modulefinder.AddPackagePath(__name__, p)
+except:
+    pass
