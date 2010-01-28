5fd85045c8a99831c10b68b715d5afec8a4e4c00
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-01-28 18:59:06 +0000

    applying patch, removing unused imports

--- enthought/traits/tests/delegate_test_case.py
+++ enthought/traits/tests/delegate_test_case.py
@@ -1,16 +1,19 @@
 #------------------------------------------------------------------------------
 # Copyright (c) 2005, Enthought, Inc.
 # All rights reserved.
-# 
+#
 # This software is provided without warranty under the terms of the BSD
 # license included in enthought/LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
 #------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+
 import unittest
 
-from enthought.traits.api import Delegate, HasTraits, Instance, Str, Any
+from ..api import Delegate, HasTraits, Instance, Str
 
 # global because event handlers are being called with wrong value for self
 baz_s_handler_self = None
@@ -24,7 +27,7 @@ class Foo( HasTraits ):
     s = Str( 'foo' )
     t = Str( 'foo.t' )
     u = Str( 'foo.u' )
-    
+
     def _s_changed(self, name, old, new):
         print 'Foo._s_changed( %s, %s, %s, %s)' % (self, name, old, new)
         global foo_s_handler_self
@@ -46,14 +49,14 @@ class BazModify( HasTraits ):
     sd = Delegate( 'foo', prefix='s', modify=True )
     t = Delegate( 'foo', modify=True )
     u = Delegate( 'foo', listenable=False, modify=True )
-    
+
     def _s_changed(self, name, old, new):
         # should never be called
         print 'BazModify._s_changed( %s, %s, %s, %s)' % (self, name, old, new)
         global baz_s_handler_self
         baz_s_handler_self = self
         return
-    
+
     def _sd_changed(self, name, old, new):
         print 'BazModify._sd_changed( %s, %s, %s, %s)' % (self, name, old, new)
         global baz_sd_handler_self
@@ -71,20 +74,20 @@ class BazModify( HasTraits ):
         global baz_u_handler_self
         baz_u_handler_self = self
         return
-    
+
 
 class BazNoModify( HasTraits ):
     foo = Instance( Foo, () )
     sd = Delegate( 'foo', prefix='s' )
     t = Delegate( 'foo' )
     u = Delegate( 'foo', listenable=False )
-    
+
     def _s_changed(self, name, old, new):
         print 'BazNoModify._s_changed( %s, %s, %s, %s)' % (self, name, old, new)
         global baz_s_handler_self
         baz_s_handler_self = self
         return
-    
+
     def _sd_changed(self, name, old, new):
         print 'BazNoModify._sd_changed( %s, %s, %s, %s)' % (self, name, old, new)
         global baz_sd_handler_self
@@ -124,7 +127,7 @@ class DelegateTestCase( unittest.TestCase ):
         Deleting the attribute should reset the trait back to its initial
         delegation behavior.
         """
-        
+
         f = Foo()
         b = Bar(foo=f)
 
@@ -152,33 +155,33 @@ class DelegateTestCase( unittest.TestCase ):
     def test_modify_prefix_handler_on_delegator(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.s, b.sd )
 
         b.sd = 'changed'
         self.assertEqual( f.s, b.sd )
-        
+
         # Don't expect _s_changed to be called because from Baz's perspective
         # the trait is named 'sd'
         self.assertEqual( baz_s_handler_self, None )
-        
+
         # Do expect '_sd_changed' to be called with b as self
         self.assertEqual( baz_sd_handler_self, b )
 
         return
-    
+
     def test_modify_prefix_handler_on_delegatee(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.s, b.sd )
-        
+
         b.sd = 'changed'
         self.assertEqual( f.s, b.sd )
-        
+
         # Foo expects its '_s_changed' handler to be called with f as self
         self.assertEqual( foo_s_handler_self, f )
-        
+
         return
 
 
@@ -186,33 +189,33 @@ class DelegateTestCase( unittest.TestCase ):
     def test_no_modify_prefix_handler_on_delegator(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.s, b.sd )
 
         b.sd = 'changed'
         self.assertNotEqual( f.s, b.sd )
-        
+
         # Don't expect _s_changed to be called because from Baz's perspective
         # the triat is named 'sd'
         self.assertEqual( baz_s_handler_self, None )
-        
+
         # Do expect '_sd_changed' to be called with b as self
         self.assertEqual( baz_sd_handler_self, b )
 
         return
-    
+
     def test_no_modify_prefix_handler_on_delegatee_not_called(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.s, b.sd )
-        
+
         b.sd = 'changed'
         self.assertNotEqual( f.s, b.sd )
-        
+
         # Foo expects its '_s_changed' handler to be called with f as self
         self.assertEqual( foo_s_handler_self, None )
-        
+
         return
 
 
@@ -221,58 +224,58 @@ class DelegateTestCase( unittest.TestCase ):
     def test_modify_handler_on_delegator(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
 
         b.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Do expect '_t_changed' to be called with b as self
         self.assertEqual( baz_t_handler_self, b )
 
         return
-    
+
     def test_modify_handler_on_delegatee(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
-        
+
         b.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Foo t did change so '_t_changed' handler should be called
         self.assertEqual( foo_t_handler_self, f)
-        
+
         return
 
 
     def test_no_modify_handler_on_delegator(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
 
         b.t = 'changed'
         self.assertNotEqual( f.t, b.t )
-        
+
         # Do expect '_t_changed' to be called with b as self
         self.assertEqual( baz_t_handler_self, b )
 
         return
-    
+
     def test_no_modify_handler_on_delegatee_not_called(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
 
         b.t = 'changed'
         self.assertNotEqual( f.t, b.t )
-        
+
         # Foo t did not change so '_t_changed' handler should not be called
         self.assertEqual( foo_t_handler_self, None)
-        
+
         return
 
 
@@ -283,29 +286,29 @@ class DelegateTestCase( unittest.TestCase ):
     def test_no_modify_handler_on_delegatee_direct_change(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
-        
+
         f.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Foo t did change so '_t_changed' handler should be called
         self.assertEqual( foo_t_handler_self, f)
-        
+
         return
 
     def test_no_modify_handler_on_delegator_direct_change(self):
         f = Foo()
         b = BazNoModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
-        
+
         f.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Do expect '_t_changed' to be called with b as self
         self.assertEqual( baz_t_handler_self, b )
-        
+
         return
 
 
@@ -316,29 +319,29 @@ class DelegateTestCase( unittest.TestCase ):
     def test_modify_handler_on_delegatee_direct_change(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
-        
+
         f.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Foo t did change so '_t_changed' handler should be called
         self.assertEqual( foo_t_handler_self, f)
-        
+
         return
 
     def test_modify_handler_on_delegator_direct_change(self):
         f = Foo()
         b = BazModify(foo=f)
-        
+
         self.assertEqual( f.t, b.t )
-        
+
         f.t = 'changed'
         self.assertEqual( f.t, b.t )
-        
+
         # Do expect '_t_changed' to be called with b as self
         self.assertEqual( baz_t_handler_self, b )
-        
+
         return
 
 
@@ -376,4 +379,4 @@ if __name__ == '__main__':
     unittest.main()
 
 #### EOF ######################################################################
-        
+
