b182c734829dba1498ba8f0fa190a5b97457f15a
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-01-28 17:34:53 +0000

    applying relative imports patch

--- enthought/traits/api.py
+++ enthought/traits/api.py
@@ -21,78 +21,66 @@ Use this module for importing Traits names into your namespace. For example::
     from enthought.traits.api import HasTraits
 """
 
-from version import __version__
+from __future__ import absolute_import
 
-from trait_base \
-    import Uninitialized, Undefined, Missing, Self, python_version
+from .version import __version__
 
-from trait_errors \
-    import TraitError, TraitNotificationError, DelegationError
+from .trait_base import Uninitialized, Undefined, Missing, Self, python_version
 
-from trait_notifiers \
-   import push_exception_handler, pop_exception_handler, \
-          TraitChangeNotifyWrapper
+from .trait_errors import TraitError, TraitNotificationError, DelegationError
 
-from category \
-    import Category
+from .trait_notifiers import (push_exception_handler, pop_exception_handler,
+        TraitChangeNotifyWrapper)
 
-from traits \
-    import CTrait, Trait, Property, TraitFactory, Default, Color, RGBColor, Font
+from .category import Category
 
-from trait_types \
-    import Any, Generic, Int, Long, Float, Complex, Str, Title, Unicode, Bool, \
-           CInt, CLong, CFloat, CComplex, CStr, CUnicode, CBool, String, \
-           Regex, Code, HTML, Password, Callable, This, self, Function, \
-           Method, Class, Module, Python, ReadOnly, Disallow, missing, \
-           Constant, Delegate, DelegatesTo, PrototypedFrom, Expression, \
-           PythonValue, File, Directory, Range, Enum, Tuple, List, CList, \
-           Set, CSet, Dict, Instance, AdaptedTo, AdaptsTo, Event, Button, \
-           ToolbarButton, Either, Type, Symbol, WeakRef, Date, Time, \
-           false, true, undefined
+from .traits import (CTrait, Trait, Property, TraitFactory, Default, Color, 
+        RGBColor, Font)
+
+from .trait_types import (Any, Generic, Int, Long, Float, Complex, Str, Title,
+        Unicode, Bool, CInt, CLong, CFloat, CComplex, CStr, CUnicode, CBool,
+        String, Regex, Code, HTML, Password, Callable, This, self, Function,
+        Method, Class, Module, Python, ReadOnly, Disallow, missing, Constant,
+        Delegate, DelegatesTo, PrototypedFrom, Expression, PythonValue, File,
+        Directory, Range, Enum, Tuple, List, CList, Set, CSet, Dict, Instance,
+        AdaptedTo, AdaptsTo, Event, Button, ToolbarButton, Either, Type,
+        Symbol, WeakRef, Date, Time, false, true, undefined)
                
-from trait_types \
-    import ListInt, ListFloat, ListStr, ListUnicode, ListComplex, ListBool, \
-           ListFunction, ListMethod, ListClass, ListInstance, ListThis, \
-           DictStrAny, DictStrStr, DictStrInt, DictStrLong, DictStrFloat, \
-           DictStrBool, DictStrList
+from .trait_types import (ListInt, ListFloat, ListStr, ListUnicode, 
+        ListComplex, ListBool, ListFunction, ListMethod, ListClass,
+        ListInstance, ListThis, DictStrAny, DictStrStr, DictStrInt,
+        DictStrLong, DictStrFloat, DictStrBool, DictStrList)
            
-from trait_types \
-    import BaseInt, BaseLong, BaseFloat, BaseComplex, BaseStr, BaseUnicode, \
-           BaseBool, BaseCInt, BaseCLong, BaseCFloat, BaseCComplex, BaseCStr, \
-           BaseCUnicode, BaseCBool, BaseFile, BaseDirectory, BaseRange, \
-           BaseEnum, BaseTuple, BaseInstance
+from .trait_types import (BaseInt, BaseLong, BaseFloat, BaseComplex, BaseStr,
+        BaseUnicode, BaseBool, BaseCInt, BaseCLong, BaseCFloat, BaseCComplex,
+        BaseCStr, BaseCUnicode, BaseCBool, BaseFile, BaseDirectory, BaseRange,
+        BaseEnum, BaseTuple, BaseInstance)
 
-if python_version >= 2.5:
-    from trait_types \
-        import UUID
+from .trait_types import UUID
 
-from has_traits \
-    import method, HasTraits, HasStrictTraits, HasPrivateTraits, Interface, \
-           SingletonHasTraits, SingletonHasStrictTraits, \
-           SingletonHasPrivateTraits, MetaHasTraits, Vetoable, VetoableEvent, \
-           implements, traits_super, on_trait_change, cached_property, \
-           property_depends_on
+from .has_traits import (method, HasTraits, HasStrictTraits, HasPrivateTraits,
+        Interface, SingletonHasTraits, SingletonHasStrictTraits,
+        SingletonHasPrivateTraits, MetaHasTraits, Vetoable, VetoableEvent,
+        implements, traits_super, on_trait_change, cached_property,
+        property_depends_on)
 
-from trait_handlers \
-    import BaseTraitHandler, TraitType, TraitHandler, TraitRange, TraitString, \
-           TraitCoerceType, TraitCastType, TraitInstance, ThisClass, \
-           TraitClass, TraitFunction, TraitEnum, TraitPrefixList, TraitMap, \
-           TraitPrefixMap, TraitCompound, TraitList, TraitListObject, \
-           TraitListEvent, TraitSetObject, TraitSetEvent, TraitDict, \
-           TraitDictObject, TraitDictEvent, TraitTuple, NO_COMPARE, \
-           OBJECT_IDENTITY_COMPARE, RICH_COMPARE           
+from .trait_handlers import (BaseTraitHandler, TraitType, TraitHandler,
+        TraitRange, TraitString, TraitCoerceType, TraitCastType, TraitInstance,
+        ThisClass, TraitClass, TraitFunction, TraitEnum, TraitPrefixList,
+        TraitMap, TraitPrefixMap, TraitCompound, TraitList, TraitListObject,
+        TraitListEvent, TraitSetObject, TraitSetEvent, TraitDict,
+        TraitDictObject, TraitDictEvent, TraitTuple, NO_COMPARE,
+        OBJECT_IDENTITY_COMPARE, RICH_COMPARE)
            
-from trait_value \
-    import BaseTraitValue, TraitValue, SyncValue, TypeValue, DefaultValue
+from .trait_value import (BaseTraitValue, TraitValue, SyncValue, 
+        TypeValue, DefaultValue)
 
-from adapter \
-    import Adapter, adapts
+from .adapter import Adapter, adapts
 
-from trait_numeric \
-    import Array, CArray
+from .trait_numeric import Array, CArray
 
 try:
-    import has_traits as has_traits
+    from . import has_traits as has_traits
     #---------------------------------------------------------------------------
     #  Patch the main traits module with the correct definition for the 
     #  ViewElements class:
@@ -102,14 +90,14 @@ try:
     #  features of traits will not work.
     #---------------------------------------------------------------------------
 
-    import ui.view_elements
-    has_traits.ViewElements = ui.view_elements.ViewElements
+    from .ui import view_elements
+    has_traits.ViewElements = view_elements.ViewElements
 
     #-------------------------------------------------------------------------------
     #  Patch the main traits module with the correct definition for the 
     #  ViewElement and ViewSubElement class:
     #-------------------------------------------------------------------------------
 
-    has_traits.ViewElement = ui.view_element.ViewElement
+    has_traits.ViewElement = view_elements.ViewElement
 except ImportError:
     pass
