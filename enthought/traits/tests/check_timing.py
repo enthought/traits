26bd848eba5b46381710356f9645681c8ae79c45
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-10 17:59:21 +0000

    applying relative imports patch, removing some unused symbols

--- enthought/traits/tests/check_timing.py
+++ enthought/traits/tests/check_timing.py
@@ -1,13 +1,13 @@
 #------------------------------------------------------------------------------
 # Copyright (c) 2005, Enthought, Inc.
 # All rights reserved.
-# 
+#
 # This software is provided without warranty under the terms of the BSD
 # license included in /LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
-# 
+#
 # Author: David C. Morrill
 # Date: 03/03/2003
 # Description: Perform timing tests on various trait styles to determine the
@@ -18,8 +18,12 @@
 #  Imports:
 #-------------------------------------------------------------------------------
 
+from __future__ import absolute_import
+
 from time import time
-from enthought.traits.api import *
+from ..api import Any, Delegate, HasTraits, Int, Range
+
+print 'OK!'
 
 #-------------------------------------------------------------------------------
 #  Constants:
@@ -32,7 +36,7 @@ n = 1000000
 t0 = -1.0
 
 #-------------------------------------------------------------------------------
-#  Measure how long it takes to execute a specified function: 
+#  Measure how long it takes to execute a specified function:
 #-------------------------------------------------------------------------------
 
 def measure ( func ):
@@ -43,13 +47,13 @@ def measure ( func ):
 #-------------------------------------------------------------------------------
 #  'Old style' Python attribute get/set:
 #-------------------------------------------------------------------------------
-    
+
 class old_style_value:
-       
+
     def measure ( self ):
         global t0
         self.init()
-       
+
         if t0 < 0.0:
             t0 = measure( self.null )
         t1 = measure( self.do_get )
@@ -59,7 +63,7 @@ class old_style_value:
         print '  get: %.2f usec' % (max( t1 - t0, 0.0 ) * scale)
         print '  set: %.2f usec' % (max( t2 - t0, 0.0 ) * scale)
         print
-       
+
     def null ( self ):
         for i in range(n):
             pass
@@ -70,21 +74,21 @@ class old_style_value:
     def do_set ( self ):
         for i in range(n):
             self.value = i
-   
+
     def do_get ( self ):
         for i in range(n):
-            self.value   
+            self.value
 
 #-------------------------------------------------------------------------------
 #  'New style' Python attribute get/set:
 #-------------------------------------------------------------------------------
-    
+
 class new_style_value ( object ):
-       
+
     def measure ( self ):
         global t0
         self.init()
-       
+
         if t0 < 0.0:
             t0 = measure( self.null )
         t1 = measure( self.do_get )
@@ -94,7 +98,7 @@ class new_style_value ( object ):
         print '  get: %.2f usec' % (max( t1 - t0, 0.0 ) * scale)
         print '  set: %.2f usec' % (max( t2 - t0, 0.0 ) * scale)
         print
-       
+
     def null ( self ):
         for i in range(n):
             pass
@@ -105,29 +109,29 @@ class new_style_value ( object ):
     def do_set ( self ):
         for i in range(n):
             self.value = i
-   
+
     def do_get ( self ):
         for i in range(n):
-            self.value   
+            self.value
 
 #-------------------------------------------------------------------------------
 #  Python 'property' get/set:
 #-------------------------------------------------------------------------------
-    
+
 class property_value ( new_style_value ):
-    
+
     def get_value ( self ):
         return self._value
 
     def set_value ( self, value ):
         self._value = value
-   
+
     value = property( get_value, set_value )
 
 #-------------------------------------------------------------------------------
 #  Python 'global' get/set:
 #-------------------------------------------------------------------------------
-    
+
 class global_value ( new_style_value ):
 
     def init ( self ):
@@ -138,42 +142,42 @@ class global_value ( new_style_value ):
         global gvalue
         for i in range(n):
             gvalue = i
-   
+
     def do_get ( self ):
         global gvalue
         for i in range(n):
-            gvalue   
- 
+            gvalue
+
 #-------------------------------------------------------------------------------
 #  Trait that can have any value:
 #-------------------------------------------------------------------------------
-  
+
 class any_value ( HasTraits, new_style_value ):
-    
+
     value = Any
-   
+
 #-------------------------------------------------------------------------------
-#  Trait that can only have 'float' values: 
+#  Trait that can only have 'float' values:
 #-------------------------------------------------------------------------------
-   
+
 class int_value ( any_value ):
-    
+
     value = Int
-   
+
 #-------------------------------------------------------------------------------
-#  Trait that can only have 'range' values: 
+#  Trait that can only have 'range' values:
 #-------------------------------------------------------------------------------
-   
+
 class range_value ( any_value ):
-    
+
     value = Range( -1, 2000000000 )
-       
+
 #-------------------------------------------------------------------------------
 #  Executes method when float trait is changed:
 #-------------------------------------------------------------------------------
-       
+
 class change_value ( int_value ):
-    
+
     def _value_changed ( self, old, new ):
         pass
 
@@ -182,45 +186,45 @@ class change_value ( int_value ):
 #-------------------------------------------------------------------------------
 
 class monitor_value ( int_value ):
-    
+
     def init ( self ):
         self.on_trait_change( self.on_value_change, 'value' )
 
     def on_value_change ( self, object, trait_name, old, new ):
         pass
-   
+
 #-------------------------------------------------------------------------------
 #  Float trait is delegated to another object:
 #-------------------------------------------------------------------------------
-   
+
 class delegate_value ( HasTraits, new_style_value ):
-    
+
     value    = Delegate( 'delegate' )
     delegate = Any
-    
+
     def init ( self ):
         self.delegate = int_value()
-   
+
 #-------------------------------------------------------------------------------
 #  Float trait is delegated through one object to another object:
 #-------------------------------------------------------------------------------
-   
+
 class delegate_2_value ( delegate_value ):
-    
+
     def init ( self ):
         self.delegate = delegate_value()
         self.delegate.init()
-         
+
 #-------------------------------------------------------------------------------
 #  Float trait is delegated through two objects to another object:
 #-------------------------------------------------------------------------------
-         
+
 class delegate_3_value ( delegate_value ):
-    
+
     def init ( self ):
         self.delegate = delegate_2_value()
         self.delegate.init()
-       
+
 #-------------------------------------------------------------------------------
 #  Run the timing measurements:
 #-------------------------------------------------------------------------------
