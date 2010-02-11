24a51dadc6b9c1913c12e6cbfc8eff96ed568db4
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-11 20:26:54 +0000

    merging VersionInfo and InfoFile into one class; adding XML creation support, and changing .info file format slightly; adding utilities to create template .info files for existing files, and for creating an updates.xml file from a list of .info files

--- enthought/util/updates/tools.py
+++ enthought/util/updates/tools.py
@@ -2,109 +2,9 @@
 files.
 """
 
-#Requires Python 2.6 (for format string notation)
-
+from enthought.util.updates.info_file import InfoFile
 import os
-import re
-import md5
 
-_version_in_name = re.compile("(\S*)[-](\d+\.\d+\.*\d*)\S*")
-
-def _get_name(filename):
-    match = _version_in_name.search(filename)
-    if match is None:
-        raise ValueError, "Could not find name in filename: {0}".format(filename)
-    return match.group(1)
-
-def _get_version(filename):
-    match = _version_in_name.search(filename)
-    if match is None:
-        raise ValueError, "Could not find version in filename: {0}".format(filename)
-    return match.group(2)
-
-def _get_checksum(filename):
-    base, ext = os.path.splitext(filename)
-    data = open(base).read()
-    return md5.new(data).hexdigest()
-
-
-name_re = re.compile('name:\s*(.*)\n')
-version_re = re.compile('version:\s*(.*)\n')
-checksum_re = re.compile('checksum:\s*(.*)\n')
-html_re = re.compile('\nhtml:')
-
-codedict = {'name':{'re':name_re,
-                    'get':_get_name},
-            'version': {'re':version_re,
-                        'get':_get_version},
-            'checksum': {'re':checksum_re,
-                         'get':_get_checksum}
-            }
-
-class InfoFile(object):
-    """Representation of an .info file
-
-    Important methods: 
-    
-    @classmethod
-    from_info_file(filename)
-
-      construct an InfoFile object from a filename --- simple parser
-
-      name: %filename% (if not present extracted from .info filename)
-      version: %filename% (if not present it is extracted from name of file)
-      checksum: md5hash (if not present it is computed from the basefile)
-      html: (everything else in the file from the next line to the end)
-    
-    get_xml()
-      return a list of xml elements for this file       
-    """
-
-    # The name of the update_file
-    name = ""
-    version = None
-    checksum = None
-
-    # A multi-line HTML document describing the changes between
-    # this version and the previous version
-    description = ""
-
-    @classmethod
-    def from_info_file(cls, filename):
-        # TODO: Change this at some point to parse using elementtree
-        str = open(filename).read()        
-        obj = cls()
-        for attr in ['name', 'version', 'checksum']:
-            funcdict = codedict[attr]
-            match = funcdict['re'].search(str)
-            if match is None:
-                value = funcdict['get'](filename)
-            else:
-                value = match.group(1)
-            setattr(obj, attr, value)
-            
-        match = html_re.search(str)
-        beg, end = match.span()
-        start = str.find('\n', end)
-        obj.description = str[start:]
-        return obj
-
-    def get_xml(self):
-        xml_elements = []
-        for attr in ['name', 'version', 'checksum', 'description']:
-            value = getattr(self, attr)
-            #str = "{spaces}<{attr}>{value}</{attr}>"
-            #xml_elements.append(str.format(spaces=" "*4,attr=attr, value=value))
-            str = "%(spaces)s<%(attr)s>%(value)s</%(attr)s>"
-            xml_elements.append(str % dict(spaces=" "*4,attr=attr, value=value))
-        return xml_elements
-
-# TODO: Change this at some point to use elementtree or something to 
-# generate real, validated XML.
-_xmlheader = """<?xml version="1.0" encoding="ISO-8859-1"?>
-<!-- DO NOT EDIT MANUALLY -->
-<!-- Automatically generated file using enthought.util.updates -->
-"""
 
 def files2xml(filenames):
     """ Given a list of filenames, extracts the app version and log
@@ -118,6 +18,12 @@ def files2xml(filenames):
     If there is no .info file for a filename or an error occurs while constructing it
     a warning message is printed. 
     """
+    
+    _xmlheader = """<?xml version="1.0" encoding="ISO-8859-1"?>
+    <!-- DO NOT EDIT MANUALLY -->
+    <!-- Automatically generated file using enthought.util.updates -->
+    """
+
     xmlparts = [_xmlheader]
     for file in filenames:
         #info_file_name = "{0}.info".format(file)
@@ -138,6 +44,7 @@ def files2xml(filenames):
         xmlparts.append('</update_file>')
 
     return "\n".join(xmlparts)
+
 
 
 
