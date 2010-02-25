48a6c8ec8f36137db50d20187fb83bbf0bb5e5b4
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-25 01:12:12 +0000

    updating versions for release

--- setup_data.py
+++ setup_data.py
@@ -20,9 +20,9 @@ def etsdep(p, min, max=None, literal=False):
 ENVISAGECORE = etsdep('EnvisageCore', '3.1.2')  # -- all from logger.(plugin|agent|widget)
 ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.1.2')  # -- all from logger.plugin
 TRAITS = etsdep('Traits', '3.2.1')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.2.1')  # -- only from e.util.traits.editor.parameter_choice_editor.py
-TRAITSGUI = etsdep('TraitsGUI', '3.1.1')  # -- from logger.(agent|plugin|widget)
-TRAITS_UI = etsdep('Traits[ui]', '3.2.1')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.3.0')  # -- only from e.util.traits.editor.parameter_choice_editor.py
+TRAITSGUI = etsdep('TraitsGUI', '3.3.0')  # -- from logger.(agent|plugin|widget)
+TRAITS_UI = etsdep('Traits[ui]', '3.3.0')
 
 # The following "soft dependencies" are wrapped in try..except blocks
 #APPTOOLS -- util/wx/drag_and_drop
