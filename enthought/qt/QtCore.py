62845279b46d6f4394e05e666fe459a427bdd358
epatters <epatters@enthought.com>
2011-04-19 14:36:35 -0500

    Add PySide-style version metadata when PyQt4 is present.

--- enthought/qt/QtCore.py
+++ enthought/qt/QtCore.py
@@ -9,5 +9,9 @@ if qt_api == 'pyqt':
     from PyQt4.Qt import QCoreApplication
     from PyQt4.Qt import Qt
 
+    # Emulate PySide version metadata.
+    __version__ = QT_VERSION_STR
+    __version_info__ = tuple(map(int, QT_VERSION_STR.split('.')))
+
 else:
     from PySide.QtCore import *
