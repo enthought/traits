4aeea5a0bbfe0cbca75e9a7f4f1d6abcc0e92547
epatters <epatters@enthought.com>
2011-05-24 13:25:07 -0500

    Add QtCore.pyqtSlot alias for PySide compatibility.

--- traits/qt/QtCore.py
+++ traits/qt/QtCore.py
@@ -6,6 +6,7 @@ if qt_api == 'pyqt':
     from PyQt4.QtCore import *
 
     from PyQt4.QtCore import pyqtSignal as Signal
+    from PyQt4.QtCore import pyqtSlot as Slot
     from PyQt4.Qt import QCoreApplication
     from PyQt4.Qt import Qt
 
