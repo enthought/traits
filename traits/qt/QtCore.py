b71dc93aed9cb33125af2247aa65e54c151b7200
epatters <epatters@enthought.com>
2011-05-25 11:42:09 -0500

    Add QtCore.pyqtProperty alias for PySide compatibility.

--- traits/qt/QtCore.py
+++ traits/qt/QtCore.py
@@ -5,6 +5,7 @@ qt_api = os.environ.get('QT_API', 'pyqt')
 if qt_api == 'pyqt':
     from PyQt4.QtCore import *
 
+    from PyQt4.QtCore import pyqtProperty as Property
     from PyQt4.QtCore import pyqtSignal as Signal
     from PyQt4.QtCore import pyqtSlot as Slot
     from PyQt4.Qt import QCoreApplication
