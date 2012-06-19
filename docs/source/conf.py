373f4b547960c56df7d5eb94619ad618e53f4b37
Ioannis Tziakos <ioannist@enthought.com>
2012-06-19 23:40:19 +0100

    update refactordoc extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,9 @@ sys.path.append(os.path.abspath('_extensions'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'refactor_doc', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+extensions = [ 'refactordoc',
+               'sphinx.ext.viewcode',
+               'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
