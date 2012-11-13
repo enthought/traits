68ad252dad7c60ecfc055a4f7e369885abc8621c
Ioannis Tziakos <ioannist@enthought.com>
2012-11-13 10:34:36 +0000

    rename MyClass -> TestObject

--- traits/testing/tests/unittest_tools_test_case.py
+++ traits/testing/tests/unittest_tools_test_case.py
@@ -13,7 +13,7 @@ from traits.api import HasTraits, Float, List, Bool, on_trait_change
 from traits.testing.api import UnittestTools
 
 
-class MyClass(HasTraits):
+class TestObject(HasTraits):
 
     number = Float(2.0)
     list_of_numbers = List(Float)
@@ -30,69 +30,69 @@ class MyClass(HasTraits):
 class UnittestToolsTestCase(unittest.TestCase, UnittestTools):
 
     def setUp(self):
-        self.my_class = MyClass()
+        self.test_object = TestObject()
 
     def test_when_using_with(self):
         """ Check normal use cases as a context manager.
         """
-        my_class = self.my_class
+        test_object = self.test_object
 
         # Change event should NOT BE detected
-        with self.assertTraitDoesNotChange(my_class, 'number') as result:
-            my_class.flag = True
-            my_class.number = 2.0
+        with self.assertTraitDoesNotChange(test_object, 'number') as result:
+            test_object.flag = True
+            test_object.number = 2.0
         self.assertEqual(result.events, [])
 
         # Change event should BE detected
-        with self.assertTraitChanges(my_class, 'number') as result:
-            my_class.flag = False
-            my_class.number = 5.0
+        with self.assertTraitChanges(test_object, 'number') as result:
+            test_object.flag = False
+            test_object.number = 5.0
 
-        expected = [(my_class, 'number', 2.0, 5.0)]
+        expected = [(test_object, 'number', 2.0, 5.0)]
         self.assertSequenceEqual(result.events, expected)
 
         # Change event should BE detected exactly 2 times
-        with self.assertTraitChanges(my_class, 'number', count=2) as result:
-            my_class.flag = False
-            my_class.number = 4.0
-            my_class.number = 3.0
+        with self.assertTraitChanges(test_object, 'number', count=2) as result:
+            test_object.flag = False
+            test_object.number = 4.0
+            test_object.number = 3.0
 
-        expected = [(my_class, 'number', 5.0, 4.0),
-                    (my_class, 'number', 4.0, 3.0)]
+        expected = [(test_object, 'number', 5.0, 4.0),
+                    (test_object, 'number', 4.0, 3.0)]
         self.assertSequenceEqual(result.events, expected)
 
         # Change event should BE detected
-        with self.assertTraitChanges(my_class, 'number') as result:
-            my_class.flag = True
-            my_class.add_to_number(10.0)
+        with self.assertTraitChanges(test_object, 'number') as result:
+            test_object.flag = True
+            test_object.add_to_number(10.0)
 
-        expected = [(my_class, 'number', 3.0, 13.0)]
+        expected = [(test_object, 'number', 3.0, 13.0)]
         self.assertSequenceEqual(result.events, expected)
 
         # Change event should BE detected exactly 3 times
-        with self.assertTraitChanges(my_class, 'number', count=3) as result:
-            my_class.flag = True
-            my_class.add_to_number(10.0)
-            my_class.add_to_number(10.0)
-            my_class.add_to_number(10.0)
+        with self.assertTraitChanges(test_object, 'number', count=3) as result:
+            test_object.flag = True
+            test_object.add_to_number(10.0)
+            test_object.add_to_number(10.0)
+            test_object.add_to_number(10.0)
 
-        expected = [(my_class, 'number', 13.0, 23.0),
-                    (my_class, 'number', 23.0, 33.0),
-                    (my_class, 'number', 33.0, 43.0)]
+        expected = [(test_object, 'number', 13.0, 23.0),
+                    (test_object, 'number', 23.0, 33.0),
+                    (test_object, 'number', 33.0, 43.0)]
         self.assertSequenceEqual(expected, result.events)
 
     def test_indirect_events(self):
         """ Check catching indirect change events.
         """
-        my_class = self.my_class
+        test_object = self.test_object
 
         # Change event should BE detected
-        with self.assertTraitChanges(my_class, 'list_of_numbers[]') as \
+        with self.assertTraitChanges(test_object, 'list_of_numbers[]') as \
                 result:
-            my_class.flag = True
-            my_class.number = -3.0
+            test_object.flag = True
+            test_object.number = -3.0
 
-        expected = [(my_class, 'list_of_numbers_items', [], [-3.0])]
+        expected = [(test_object, 'list_of_numbers_items', [], [-3.0])]
         self.assertSequenceEqual(result.events, expected)
 
     def test_exception_inside_context(self):
@@ -100,72 +100,72 @@ class UnittestToolsTestCase(unittest.TestCase, UnittestTools):
         propagated.
 
         """
-        my_class = self.my_class
+        test_object = self.test_object
 
         with self.assertRaises(AttributeError):
-            with self.assertTraitChanges(my_class, 'number'):
-                my_class.i_do_exist
+            with self.assertTraitChanges(test_object, 'number'):
+                test_object.i_do_exist
 
         with self.assertRaises(AttributeError):
-            with self.assertTraitDoesNotChange(my_class, 'number'):
-                my_class.i_do_exist
+            with self.assertTraitDoesNotChange(test_object, 'number'):
+                test_object.i_do_exist
 
     def test_non_change_on_failure(self):
         """ Check behaviour when assertion should be raised for non trait
         change.
 
         """
-        my_class = self.my_class
+        test_object = self.test_object
         traits = 'flag, number'
         with self.assertRaises(AssertionError):
-            with self.assertTraitDoesNotChange(my_class, traits) as result:
-                my_class.flag = True
-                my_class.number = -3.0
-        expected = [(my_class, 'flag', False, True),
-                    (my_class, 'number', 2.0, -3.0)]
+            with self.assertTraitDoesNotChange(test_object, traits) as result:
+                test_object.flag = True
+                test_object.number = -3.0
+        expected = [(test_object, 'flag', False, True),
+                    (test_object, 'number', 2.0, -3.0)]
         self.assertEqual(result.events, expected)
 
     def test_change_on_failure(self):
         """ Check behaviour when assertion should be raised for trait change.
         """
-        my_class = self.my_class
+        test_object = self.test_object
         with self.assertRaises(AssertionError):
-            with self.assertTraitChanges(my_class, 'number') as result:
-                my_class.flag = True
+            with self.assertTraitChanges(test_object, 'number') as result:
+                test_object.flag = True
         self.assertEqual(result.events, [])
 
         # Change event will not be fired 3 times
         with self.assertRaises(AssertionError):
-            with self.assertTraitChanges(my_class, 'number', count=3) as \
+            with self.assertTraitChanges(test_object, 'number', count=3) as \
                     result:
-                my_class.flag = True
-                my_class.add_to_number(10.0)
-                my_class.add_to_number(10.0)
+                test_object.flag = True
+                test_object.add_to_number(10.0)
+                test_object.add_to_number(10.0)
 
-        expected = [(my_class, 'number', 2.0, 12.0),
-                    (my_class, 'number', 12.0, 22.0)]
+        expected = [(test_object, 'number', 2.0, 12.0),
+                    (test_object, 'number', 12.0, 22.0)]
         self.assertSequenceEqual(expected, result.events)
 
     def test_asserts_in_context_block(self):
         """ Make sure that the traits context manager does not stop
         regular assertions inside the managed code block from happening.
         """
-        my_class = MyClass(number=16.0)
+        test_object = TestObject(number=16.0)
 
-        with self.assertTraitDoesNotChange(my_class, 'number'):
-            self.assertEqual(my_class.number, 16.0)
+        with self.assertTraitDoesNotChange(test_object, 'number'):
+            self.assertEqual(test_object.number, 16.0)
 
         with self.assertRaisesRegexp(AssertionError, '16\.0 != 12\.0'):
-            with self.assertTraitDoesNotChange(my_class, 'number'):
-                self.assertEqual(my_class.number, 12.0)
+            with self.assertTraitDoesNotChange(test_object, 'number'):
+                self.assertEqual(test_object.number, 12.0)
 
     def test_special_case_for_count(self):
         """ Count equal to 0 should be valid but it is discouraged.
         """
-        my_class = MyClass(number=16.0)
+        test_object = TestObject(number=16.0)
 
-        with self.assertTraitChanges(my_class, 'number', count=0):
-            my_class.flag = True
+        with self.assertTraitChanges(test_object, 'number', count=0):
+            test_object.flag = True
 
 
 if __name__ == '__main__':
