d660f0342b1edfcbb2861f1a9db890a91f0fe868
Pietro Berkes <pberkes@enthought.com>
2013-04-05 14:06:29 +0100

    'api' module in adaptation package.

---
+++ api.py
@@ -0,0 +1,4 @@
+from .adaptation_manager import adapt, AdaptationError, \
+    register_adapter_factory, register_adaptation_offer, supports_protocol
+
+from .adaptation_offer import AdaptationOffer
