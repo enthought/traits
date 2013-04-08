0ae45e7c50ae7aef3119c4a4dc1edbbf3acf8f9d
Pietro Berkes <pberkes@enthought.com>
2013-04-08 09:08:51 +0100

    Benchmark: two-levels deep adaptation.

--- tests/adaptation_benchmark.py
+++ tests/adaptation_benchmark.py
@@ -9,7 +9,7 @@ from apptools.adaptation.adaptation_manager import AdaptationManager
 from traits.api import HasTraits, implements, Interface
 
 
-N_ITERATIONS = 100000
+N_ITERATIONS = 10000
 
 
 class IFoo(Interface):
@@ -18,8 +18,14 @@ class IFoo(Interface):
 class IBar(Interface):
     pass
 
+class IBaz(Interface):
+    pass
+
 class IFooToIBar(HasTraitsAdapter):
     implements(IBar)
+
+class IBarToIBaz(HasTraitsAdapter):
+    implements(IBaz)
 
 class Foo(HasTraits):
     implements(IFoo)
@@ -29,10 +35,11 @@ foo = Foo()
 # PyProtocols
 from traits.api import adapts as traits_register
 traits_register(IFooToIBar, IFoo, IBar)
+traits_register(IBarToIBaz, IBar, IBaz)
 
 start_time = time.time()
 for _ in range(N_ITERATIONS):
-    IBar(foo)
+    IBaz(foo)
 time_per_iter = (time.time() - start_time) / float(N_ITERATIONS) * 1000.0
 print 'traits.protocol: %f msec per iteration' % time_per_iter
 
@@ -43,10 +50,15 @@ adaptation_manager.register_adapter_factory(
     from_protocol = IFoo,
     to_protocol   = IBar
 )
+adaptation_manager.register_adapter_factory(
+    factory       = IBarToIBaz,
+    from_protocol = IBar,
+    to_protocol   = IBaz
+)
 
 start_time = time.time()
 for _ in range(N_ITERATIONS):
-    adaptation_manager.adapt(foo, IBar)
+    adaptation_manager.adapt(foo, IBaz)
 time_per_iter = (time.time() - start_time) / float(N_ITERATIONS) * 1000.0
 print 'apptools using Interfaces: %.3f msec per iteration' % time_per_iter
 
@@ -57,11 +69,20 @@ class FooABC(object):
 class BarABC(object):
     __metaclass__ = abc.ABCMeta
 
+class BazABC(object):
+    __metaclass__ = abc.ABCMeta
+
 class FooABCToBarABC(object):
     def __init__(self, adaptee):
         pass
 
 BarABC.register(FooABCToBarABC)
+
+class BarABCToBazABC(object):
+    def __init__(self, adaptee):
+        pass
+
+BazABC.register(BarABCToBazABC)
 
 class ConcreteFoo(object):
     pass
@@ -76,9 +97,14 @@ adaptation_manager.register_adapter_factory(
     from_protocol = FooABC,
     to_protocol   = BarABC
 )
+adaptation_manager.register_adapter_factory(
+    factory       = BarABCToBazABC,
+    from_protocol = BarABC,
+    to_protocol   = BazABC
+)
 
 start_time = time.time()
 for _ in range(N_ITERATIONS):
-    adaptation_manager.adapt(concrete_foo, BarABC)
+    adaptation_manager.adapt(concrete_foo, BazABC)
 time_per_iter = (time.time() - start_time) / float(N_ITERATIONS) * 1000.0
 print 'apptools using ABCs: %.3f msec per iteration' % time_per_iter
