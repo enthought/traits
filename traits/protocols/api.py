d35206c7a3ce59a864990acb7fa88ddc4d4e628d
Pietro Berkes <pberkes@enthought.com>
2013-04-25 16:41:02 +0100

    Remove pyprotocols, provide replacement for things on the api.
    All the replacement functions and classes are marked as deprecated.

--- traits/protocols/api.py
+++ traits/protocols/api.py
@@ -6,9 +6,44 @@ on external packages from the Traits package. The only significant change is
 the inclusion of a setup.py file.
 """
 
-from __future__ import absolute_import
+from traits.util.api import deprecated
 
-from .protocols import (adapt, declareAdapterForType, declareAdapterForProtocol,
-    declareAdapterForObject, advise, declareImplementation, declareAdapter,
-    adviseObject, InterfaceClass, Protocol, addClassAdvisor, AdaptationFailure)
+@deprecated("use the 'adapt' function in 'traits.adaptation' instead")
+def adapt(*args, **kw):
+    from apptools.adaptation.api import adapt
 
+    return adapt(*args, **kw)
+
+@deprecated("use the 'register_factory' function in 'traits.adaptation' instead")
+def declareAdapter(factory, provides,
+                   forTypes=(), forProtocols=(), forObjects=()):
+
+    from apptools.adaptation.api import register_factory
+    from itertools import chain
+
+    for from_protocol in chain(forTypes, forProtocols, forObjects):
+        for to_protocol in provides:
+            register_factory(factory, from_protocol, to_protocol)
+
+@deprecated("use ABC's 'register method, or declare a null adapter instead")
+def declareImplementation(protocol,
+                          instancesProvide=(), instancesDoNotProvide=()):
+
+    from apptools.adaptation.api import register_factory
+
+    def null_adapter(adaptee):
+        return adaptee
+
+    for to_protocol in instancesProvide:
+        register_factory(null_adapter, protocol, to_protocol)
+
+from apptools.adaptation.adaptation_error import AdaptationError \
+    as AdaptationFailure
+
+AdaptationFailure = deprecated(
+    "use 'AdaptationError' in 'traits.adaptation' instead"
+)(AdaptationFailure)
+
+# We will provide decorators as replacements for 'implements' and 'adapts'
+# in the future.
+from .advice import addClassAdvisor
