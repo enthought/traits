beb8035cf1fa750a151b470d139350254267c242
Ioannis Tziakos <ioannist@enthought.com>
2013-09-27 22:50:05 +0100

    update imports to not use the `api` module

--- traits/tests/dict_test_case.py
+++ traits/tests/dict_test_case.py
@@ -18,7 +18,9 @@ from __future__ import absolute_import
 
 from traits.testing.unittest_tools import unittest
 
-from ..api import on_trait_change, Dict, Event, HasTraits, Str
+from ..trait_types import Dict, Event, Str, TraitDictObject
+from ..has_traits import HasTraits, on_trait_change
+from ..trait_errors import TraitError
 
 
 # fixme: We'd like to use a callable instance for the listener so that we
