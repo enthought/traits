ace62a36a54fdef58fb915ef18841c60e52df122
Ioannis Tziakos <ioannist@enthought.com>
2014-02-10 10:19:42 +0000

    augment test to check that we get a separate greenlet per event

--- traits/tests/test_gevent_notifiers.py
+++ traits/tests/test_gevent_notifiers.py
@@ -1,11 +1,11 @@
-""" Tests for dynamic notifiers with `dispatch='greenlet'`.
+""" Tests for dynamic notifiers with `dispatch='gevent'`.
 
-Dynamic notifiers created with the `dispatch='ui'` option dispatch event
-notifications on the UI thread. The class handling the dispatch,
-`FastUITraitChangeNotifyWrapper`, is a subclass of `TraitChangeNotifyWrapper`.
+Dynamic notifiers created with the `dispatch='gevent'` option dispatch event
+notifications on the gevent event loop. The class handling the dispatch,
+`GEventTraitChangeNotifyWrapper`, is a subclass of `TraitChangeNotifyWrapper`.
 Most of the functionality of the class is thus already covered by the
 `TestDynamicNotifiers` test case, and we only need to test that the
-notification really occurs on the UI thread.
+notification really occurs on the gevent eventloop.
 
 At present, `dispatch='ui'` and `dispatch='fast_ui'` have the same effect.
 
@@ -14,6 +14,7 @@ import gevent
 
 from traits.api import Float, HasTraits, List
 from traits.testing.unittest_tools import unittest, UnittestTools
+
 
 class Foo(HasTraits):
     foo = Float
@@ -27,11 +28,13 @@ class TestGEventNotifiers(unittest.TestCase, UnittestTools):
 
     def setUp(self):
         self.notifications = []
+        self.greenlets = set()
 
     def on_foo_notifications(self, obj, name, old, new):
         if name == 'foo':
             gevent.sleep(1)
         event = (obj, name, old, new)
+        self.greenlets.add(gevent.getcurrent())
         self.notifications.append(event)
 
     #### Tests ################################################################
@@ -49,6 +52,12 @@ class TestGEventNotifiers(unittest.TestCase, UnittestTools):
             obj.foo = float(i)
             obj.boo = float(i)
         gevent.wait()
+
+        # Each event should be a separate greenlet
+        self.assertEqual(len(self.greenlets), 18)
+
+        # Event should arrive out of order when some of the listeners
+        # release the thread.
         self.assertEqual(len(self.notifications), 18)
         previous = None
         for _, name, _, _ in self.notifications:
@@ -59,7 +68,7 @@ class TestGEventNotifiers(unittest.TestCase, UnittestTools):
         else:
             self.fail(
                 " We should have had some events out of order. "
-                " Since half of the are sleeping for 1 second. ")
+                " Since half of them are sleeping for 1 second. ")
 
 
 if __name__ == '__main__':
