7cdedac097dcc2023f5545a5a7562122457cb723
Ioannis Tziakos <ioannist@enthought.com>
2014-02-19 17:36:43 +0000

    rework the docstrings and copyright

--- traits/util/event_tracer.py
+++ traits/util/event_tracer.py
@@ -1,50 +1,18 @@
-# This software is OSI Certified Open Source Software.
-# OSI Certified is a certification mark of the Open Source Initiative.
+#------------------------------------------------------------------------------
 #
-# Copyright (c) 2013, Enthought, Inc.
-# All rights reserved.
+#  Copyright (c) 2013, Enthought, Inc.
+#  All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
 #
-#  * Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#  * Neither the name of Enthought, Inc. nor the names of its contributors may
-#    be used to endorse or promote products derived from this software without
-#    specific prior written permission.
+#  Thanks for using Enthought open source!
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""Multi-threaded trait event tracer.
-
-Place this script somewhere importable
-(e.g. C:\\Python27\\Lib\\site-packages\\).
-
-Use it as follows::
-
-    >>> from trace_recorder import record_events
-    >>> with record_events('C:\\dev\\trace'):
-    ...     my_model.some_trait = True
-
-This will install a tracer that will record all events that occur from
-setting of some_trait on the my_model instance.
-
-The results will be stored in one file per running thread in the
-directory 'C:\\dev\\trace'.  The files are named after the thread being
-traced.
-
-"""
+#  Author: Simon Jagoe
+#
+#------------------------------------------------------------------------------
 from contextlib import contextmanager
 import inspect
 import os
@@ -203,12 +171,28 @@ class ThreadChangeEventRecorder(object):
 
 @contextmanager
 def record_events(trace_directory):
-    """ Record trait change events.
+    """ Multi-threaded trait event tracer
 
     Parameters
     ----------
     trace_directory : string
         The directory where the change log for each event will be saved
+
+
+    Usage
+    -----
+    ::
+
+        >>> from trace_recorder import record_events
+        >>> with record_events('C:\\dev\\trace'):
+        ...     my_model.some_trait = True
+
+    This will install a tracer that will record all events that occur from
+    setting of some_trait on the my_model instance.
+
+    The results will be stored in one file per running thread in the
+    directory 'C:\\dev\\trace'.  The files are named after the thread being
+    traced.
 
     """
     recorder = ChangeEventRecorder(trace_directory)
