07be237e68bb7869eaf06edaea29d9ae17caacbc
Ioannis Tziakos <ioannist@enthought.com>
2014-02-20 15:50:32 +0000

    provide reference to the related blog in the dcostring

--- fixes/fix_unicode_methods.py
+++ fixes/fix_unicode_methods.py
@@ -3,6 +3,12 @@ from lib2to3.fixer_util import Name
 
 
 class FixUnicodeMethods(fixer_base.BaseFix):
+    """ Custom fixer for the __unicode__ method
+
+    Code used from:
+    http://lucumr.pocoo.org/2010/2/11/porting-to-python-3-a-guide/
+
+    """
     PATTERN = r"funcdef< 'def' name='__unicode__' parameters< '(' NAME ')' > any+ >"
 
     def transform(self, node, results):
