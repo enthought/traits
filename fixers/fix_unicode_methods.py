a1016dc4129e3b2408d3fa78d22daf67671260bb
Ioannis Tziakos <ioannist@enthought.com>
2014-02-20 17:27:12 +0000

    pep8 and pyflakes

--- fixers/fix_unicode_methods.py
+++ fixers/fix_unicode_methods.py
@@ -9,7 +9,7 @@ class FixUnicodeMethods(fixer_base.BaseFix):
     http://lucumr.pocoo.org/2010/2/11/porting-to-python-3-a-guide/
 
     """
-    PATTERN = r"funcdef< 'def' name='__unicode__' parameters< '(' NAME ')' > any+ >"
+    PATTERN = r"funcdef< 'def' name='__unicode__' parameters< '(' NAME ')' > any+ >"  # noqa
 
     def transform(self, node, results):
         name = results['name']
