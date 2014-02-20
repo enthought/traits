3e4af03002fd50a63baf4f3e1e483be6dbb66d43
Ioannis Tziakos <ioannist@enthought.com>
2014-02-20 15:11:21 +0000

    move the fixer into a derectory

--- fixes/fix_unicode_methods.py
+++ fixes/fix_unicode_methods.py
@@ -2,10 +2,9 @@ from lib2to3 import fixer_base
 from lib2to3.fixer_util import Name
 
 
-class FixRenameUnicode(fixer_base.BaseFix):
+class FixUnicodeMethods(fixer_base.BaseFix):
     PATTERN = r"funcdef< 'def' name='__unicode__' parameters< '(' NAME ')' > any+ >"
 
     def transform(self, node, results):
-        print('I was called')
         name = results['name']
         name.replace(Name('__str__', prefix=name.prefix))
