603e5edb9b461126f4e648a15d558fdaef96de3d
Mark Dickinson <mdickinson@enthought.com>
2014-05-08 12:44:11 +0100

    Partial work on test cleanup.

--- traits/tests/test_events.py
+++ traits/tests/test_events.py
@@ -12,6 +12,8 @@
 
 from __future__ import absolute_import
 
+import unittest
+
 from ..api import HasTraits
 
 
@@ -21,34 +23,34 @@ class TestBase(HasTraits):
         't2': 0
     }
 
+    def __init__(self, **traits):
+        HasTraits.__init__(self, **traits)
+        self.events = []
+
     def test(self):
-        print '---------- Begin %s test ----------' % self.__class__.__name__
-        print 'normal changes'
         self.t1 = 1
         self.t2 = 2
-
-        print '---------- End %s test ----------\n' % self.__class__.__name__
 
 
 class Test1(TestBase):
 
     def t1_changed(self, old, new):
-        print 't1 changed:', old, new
+        self.events.append(('t1 changed:', old, new))
 
     def t2_changed(self, old, new):
-        print 't2 changed:', old, new
+        self.events.append(('t2 changed:', old, new))
 
 
 class Test2(Test1):
 
     def anytrait_changed(self, name, old, new):
-        print 'anytrait changed:', name, old, new
+        self.events.append(('anytrait changed:', name, old, new))
 
 
 class Test3(TestBase):
 
     def anytrait_changed(self, name, old, new):
-        print 'anytrait changed:', name, old, new
+        self.events.append(('anytrait changed:', name, old, new))
 
 
 class Test4(TestBase):
@@ -58,7 +60,7 @@ class Test4(TestBase):
         self.on_trait_change(self.on_anytrait)
 
     def on_anytrait(self, object, name, old, new):
-        print 'on anytrait changed:', name, old, new
+        self.events.append(('on anytrait changed:', name, old, new))
 
 
 class Test5(TestBase):
@@ -69,10 +71,10 @@ class Test5(TestBase):
         self.on_trait_change(self.t2_trait, 't2')
 
     def t1_trait(self, object, name, old, new):
-        print 'on t1 changed:', old, new
+        self.events.append(('on t1 changed:', old, new))
 
     def t2_trait(self, object, name, old, new):
-        print 'on t2 changed:', old, new
+        self.events.append(('on t2 changed:', old, new))
 
 
 class Test6(Test5):
@@ -82,7 +84,7 @@ class Test6(Test5):
         self.on_trait_change(self.on_anytrait)
 
     def on_anytrait(self, object, name, old, new):
-        print 'on anytrait changed:', name, old, new
+        self.events.append(('on anytrait changed:', name, old, new))
 
 
 class Test7(Test1):
@@ -93,50 +95,79 @@ class Test7(Test1):
         self.on_trait_change(self.t2_trait, 't2')
 
     def t1_trait(self, object, name, old, new):
-        print 'on t1 changed:', old, new
+        self.events.append(('on t1 changed:', old, new))
 
     def t2_trait(self, object, name, old, new):
-        print 'on t2 changed:', old, new
+        self.events.append(('on t2 changed:', old, new))
 
 
 class Test8(Test2):
 
     def __init__(self, **traits):
-        Test1.__init__(self, **traits)
+        Test2.__init__(self, **traits)
         self.on_trait_change(self.t1_trait, 't1')
         self.on_trait_change(self.t2_trait, 't2')
         self.on_trait_change(self.on_anytrait)
 
     def on_anytrait(self, object, name, old, new):
-        print 'on anytrait changed:', name, old, new
+        self.events.append(('on anytrait changed:', name, old, new))
 
     def t1_trait(self, object, name, old, new):
-        print 'on t1 changed:', old, new
+        self.events.append(('on t1 changed:', old, new))
 
     def t2_trait(self, object, name, old, new):
-        print 'on t2 changed:', old, new
+        self.events.append(('on t2 changed:', old, new))
 
 
-test1 = Test1()
-test1.test()
+class TestEvents(unittest.TestCase):
+    def test_events_1(self):
+        test = Test1()
+        test.test()
+        self.assertEqual(test.events, [])
 
-test2 = Test2()
-test2.test()
+    def test_events_2(self):
+        test = Test2()
+        test.test()
+        self.assertEqual(test.events, [])
 
-test3 = Test3()
-test3.test()
+    def test_events_3(self):
+        test = Test3()
+        test.test()
+        self.assertEqual(test.events, [])
 
-test4 = Test4()
-test4.test()
+    def test_events_4(self):
+        test = Test4()
+        test.test()
+        events = test.events
+        self.assertEqual(len(events), 2)
+        for event in events:
+            self.assertEqual(event[0], 'on anytrait changed:')
+            self.assertEqual(event[1], 'trait_added')
 
-test5 = Test5()
-test5.test()
+        # Compare sets to avoid making any assumptions about event ordering.
+        new_values = set([event[3] for event in events])
+        self.assertEqual(new_values, set(['t1', 't2']))
 
-test6 = Test6()
-test6.test()
+    def test_events_5(self):
+        test = Test5()
+        test.test()
+        self.assertEqual(test.events, [])
 
-test7 = Test7()
-test7.test()
+    def test_events_6(self):
+        test = Test6()
+        test.test()
+        self.assertEqual(test.events, [])
 
-test8 = Test8()
-test8.test()
+    def test_events_7(self):
+        test = Test7()
+        self.assertEqual(test.events, [])
+        test.test()
+
+    def test_events_8(self):
+        test = Test8()
+        self.assertEqual(test.events, [])
+        test.test()
+
+
+if __name__ == '__main__':
+    unittest.main()
