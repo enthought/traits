8fed0d4174a395b9b3b0d00adb6aab08f158076c
Mark Dickinson <mdickinson@enthought.com>
2014-12-17 13:09:26 +0000

    Add tests for non-sharing and validation of the default value.

--- traits/tests/test_array_or_none.py
+++ traits/tests/test_array_or_none.py
@@ -61,6 +61,13 @@ class TestArrayOrNone(unittest.TestCase, UnittestTools):
         foo = Foo()
         self.assertIsInstance(foo.maybe_array_with_default, numpy.ndarray)
 
+    def test_default_validation(self):
+        # CArray and Array validate the default at class creation time;
+        # we do the same for ArrayOrNone.
+        with self.assertRaises(TraitError):
+            class Bar(HasTraits):
+                bad_array = ArrayOrNone(shape=(None, None), value=[1, 2, 3])
+
     def test_setting_array_from_array(self):
         foo = Foo()
         test_array = numpy.arange(5)
@@ -146,3 +153,25 @@ class TestArrayOrNone(unittest.TestCase, UnittestTools):
 
         with self.assertTraitChanges(foo, 'maybe_array_no_compare'):
             foo.maybe_array_no_compare = test_array
+
+    def test_default_value_copied(self):
+        # Check that we don't share defaults.
+        test_default = numpy.arange(100.0, 110.0)
+
+        class FooBar(HasTraits):
+            foo = ArrayOrNone(value=test_default)
+
+            bar = ArrayOrNone(value=test_default)
+
+        foo_bar = FooBar()
+
+        self.assertTrue((foo_bar.foo == test_default).all())
+        self.assertTrue((foo_bar.bar == test_default).all())
+
+        test_default += 2.0
+        self.assertFalse((foo_bar.foo == test_default).all())
+        self.assertFalse((foo_bar.bar == test_default).all())
+
+        foo = foo_bar.foo
+        foo += 1729.0
+        self.assertFalse((foo_bar.foo == foo_bar.bar).all())
