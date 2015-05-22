ab4c5a126a7d459b3fb5e6a20cae2cf60b184d56
Jonathan Rocher <jrocher@enthought.com>
2015-05-21 22:12:05 -0500

    Made the exception message building work for Python 2.6, 2.7 and 3.x

--- traits/tests/test_trait_exceptions.py
+++ traits/tests/test_trait_exceptions.py
@@ -29,5 +29,6 @@ class TestGetAttr(unittest.TestCase):
             self.a.__getattribute__(2)
 
         # Error message contains value and type of bad attribute name
-        self.assertIn("2", e.exception.message)
-        self.assertIn("int", e.exception.message)
+        exception_msg = str(e.exception)
+        self.assertIn("2", exception_msg)
+        self.assertIn("int", exception_msg)
