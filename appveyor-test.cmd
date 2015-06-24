5a158676e50efce9df825c56253cc93382b13590
Ioannis Tziakos <ioannist@enthought.com>
2015-06-25 00:35:44 +0100

    use a batch script to allow cython tests to run

---
+++ appveyor-test.cmd
@@ -0,0 +1,9 @@
+"%sdkverpath%" -q -version:"%sdkver%"
+call setenv /x64
+
+mkdir testrun
+copy .coveragerc testrun
+cd testrun
+coverage run -m nose.core -v traits --exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+coverage report
