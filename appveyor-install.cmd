22ead9de95e2d4e47e959ec9c5cb7c0ed2821d87
Ioannis Tziakos <ioannist@enthought.com>
2015-06-24 18:53:17 +0100

    install numpy

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -4,6 +4,7 @@ call setenv /x64
 rem install python packages
 pip install --cache-dir c:/egg_cache nose
 pip install --cache-dir c:/egg_cache coverage
+pip install --cache-dir c:/egg_cache numpy
 
 rem install mayavi
 python setup.py develop
