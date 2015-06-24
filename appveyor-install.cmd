a6a133484a8ed309b995d69abf5b7f84c40c1b93
Ioannis Tziakos <ioannist@enthought.com>
2015-06-24 22:05:39 +0100

    install cython

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -5,6 +5,7 @@ rem install python packages
 pip install --cache-dir c:/egg_cache nose
 pip install --cache-dir c:/egg_cache coverage
 pip install --cache-dir c:/egg_cache numpy
+pip install --cache-dir c:/egg_cache cython
 
 rem install mayavi
 python setup.py develop
