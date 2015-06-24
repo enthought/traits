22949ef093742bbfc7fd99c6de7c9002faf95f46
Ioannis Tziakos <ioannist@enthought.com>
2015-06-24 18:08:55 +0100

    fix cache folder name

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -2,8 +2,8 @@
 call setenv /x64
 
 rem install python packages
-pip install --cache-dir c:/temp nose
-pip install --cache-dir c:/temp coverage
+pip install --cache-dir c:/egg_cache nose
+pip install --cache-dir c:/egg_cache coverage
 
 rem install mayavi
 python setup.py develop
