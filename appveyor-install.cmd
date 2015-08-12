382b6e96b23d825903a0acee95ef76d86e8ae1ae
Mark Dickinson <mdickinson@enthought.com>
2015-08-12 20:29:26 +0100

    Attempt to fix Appveyor failures on Python 3, which appear to be due to a bug in the newest babel.

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -2,10 +2,11 @@
 call setenv /x64
 
 rem install python packages
-pip install --cache-dir c:/egg_cache nose
-pip install --cache-dir c:/egg_cache coverage
-pip install --cache-dir c:/egg_cache numpy
-pip install --cache-dir c:/egg_cache cython
+pip install --cache-dir C:/egg_cache nose
+pip install --cache-dir C:/egg_cache coverage
+pip install --cache-dir C:/egg_cache numpy
+pip install --cache-dir C:/egg_cache cython
+pip install --cache-dir C:/egg_cache babel==1.3
 pip install --cache-dir C:/egg_cache Sphinx
 
 rem install traits
