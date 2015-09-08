63b455a14fcea87ef519eec3a6c7d37d9ea0df5c
ferdonline <ferdonline@gmail.com>
2015-09-08 15:16:41 +0200

    Testing the new selector function for traits to be shown in default View

--- traits/tests/test_get_traits.py
+++ traits/tests/test_get_traits.py
@@ -22,6 +22,12 @@ from ..api import HasTraits, Int, Str, Undefined, ReadOnly, Float
 class Foo(HasTraits):
     num = Int
     bar = Str
+
+class Bar(HasTraits):
+    PubT1   = Str                     #Default is public visible
+    PubT1   = Str( visible=False )    #Change to invisible
+    PrivT1  = Str( private=True )     #New behavior sets private Traits invisible
+    PrivT2  = Str( private=True, visible=True ) #Make private traits visible
     
 
 class GetTraitTestCase(unittest.TestCase):
@@ -29,14 +35,14 @@ class GetTraitTestCase(unittest.TestCase):
     @unittest.expectedFailure
     def test_trait_set_bad(self):
         b = Foo()
-        b.num = 'first'
+        b.num = 'first'                         #This shall obviously fail (old and new behavior)
         self.failUnlessEqual(b.num, 'first')
         return
     
     def test_trait_set_replaced(self):
         b = Foo()
-        b.add_trait( "num", Str() )
-        b.num = 'first'
+        b.add_trait( "num", Str() )             #Overriding the trait with a new type
+        b.num = 'first'                         #Will work (not working before)
         self.failUnlessEqual(b.num, 'first')
         return
     
@@ -48,12 +54,14 @@ class GetTraitTestCase(unittest.TestCase):
         self.failUnlessEqual(b.num, 'first')
         
         #Should mean either way we ask for it we get the new one
-        self.failUnlessEqual( b.trait("num"), b.traits()["num"] )
-        
+        self.failUnlessEqual( b.trait("num"), b.traits()["num"] )    #Before the functions would return different traits!
         return
     
     
-
+    def test_trait_names_returned_by_visible_traits(self):
+        b = Bar()
+        self.failUnlessEqual( b.visible_traits(), ["PubT1", "PrivT2"] )
+        return
 
 
 ### EOF #######################################################################
