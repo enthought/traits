6a2956f2c62db2bc3e46c7297245620631e9a5b7
Mark Dickinson <mdickinson@enthought.com>
2015-09-22 19:38:03 +0100

    Downgrade coverage to see if that helps the Appveyor builds.

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -3,7 +3,7 @@ call setenv /x64
 
 rem install python packages
 pip install --cache-dir C:/egg_cache nose
-pip install --cache-dir C:/egg_cache coverage
+pip install --cache-dir C:/egg_cache coverage==3.7.1
 pip install --cache-dir C:/egg_cache numpy
 pip install --cache-dir C:/egg_cache cython
 rem Work around bug in babel 2.0: see mitsuhiko/babel#174
