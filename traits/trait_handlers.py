eab0984542fbb56476ef056f31284e41d955654e
Corran Webster <cwebster@enthought.com>
2016-03-16 15:01:24 +0000

    Fix in-place trait set operations.

--- traits/trait_handlers.py
+++ traits/trait_handlers.py
@@ -2757,6 +2757,7 @@ class TraitSetObject ( set ):
         if not hasattr(self, 'trait'):
             return set.update(self, value)
         try:
+            value = set(value)
             added = value.difference( self )
             if len( added ) > 0:
                 object   = self.object()
@@ -2796,6 +2797,7 @@ class TraitSetObject ( set ):
     def symmetric_difference_update ( self, value ):
         if not hasattr(self, 'trait'):
             return set.symmetric_difference_update(self, value)
+        value = set(value)
         removed = self.intersection( value )
         added   = value.difference( self )
         if (len( removed ) > 0) or (len( added ) > 0):
