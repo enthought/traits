cf42547a661658d7ed4267b590a966fecf201ef2
Mark Dickinson <mdickinson@enthought.com>
2016-06-06 14:18:19 +0100

    Add tests for slice assignments where the RHS has a different length from the slice.

--- traits/tests/test_list.py
+++ traits/tests/test_list.py
@@ -110,6 +110,28 @@ class ListTestCase(unittest.TestCase):
             self.assertEqual(
                 f.l, plain_l, "failed for slice {0!r}".format(test_slice))
 
+    def test_slice_assignments_of_different_length(self):
+        # Test slice assignments where rhs has a different length
+        # to the slice. These should work only for slices of step 1.
+        test_list = ['zero', 'one', 'two', 'three']
+        f = Foo(l=test_list)
+        f.l[1:3] = '01234'
+        self.assertEqual(f.l, ['zero', '0', '1', '2', '3', '4', 'three'])
+        f.l[4:] = []
+        self.assertEqual(f.l, ['zero', '0', '1', '2'])
+        f.l[:] = 'abcde'
+        self.assertEqual(f.l, ['a', 'b', 'c', 'd', 'e'])
+        f.l[:] = []
+        self.assertEqual(f.l, [])
+
+        f = Foo(l=test_list)
+        with self.assertRaises(ValueError):
+            f.l[::2] = ['a', 'b', 'c']
+        self.assertEqual(f.l, test_list)
+        with self.assertRaises(ValueError):
+            f.l[::-1] = []
+        self.assertEqual(f.l, test_list)
+
     def test_slice_deletion_bad_length_computation(self):
         # Regression test for enthought/traits#283.
         class IHasConstrainedList(HasTraits):
