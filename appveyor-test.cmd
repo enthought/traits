2ca9e520db075e35ccb411a3778f1b83a47d53e6
Ioannis Tziakos <ioannist@enthought.com>
2017-01-13 15:49:37 +0000

    remove batch file

--- appveyor-test.cmd
+++
@@ -1,9 +0,0 @@
-"%sdkverpath%" -q -version:"%sdkver%"
-call setenv /x64
-
-mkdir testrun
-copy .coveragerc testrun
-cd testrun
-coverage run -m nose.core -v traits --exe
-if %errorlevel% neq 0 exit /b %errorlevel%
-coverage report
