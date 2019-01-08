831d0c6782a7b6379f2a509909da9227d5b83588
James Johnson <jjohnson@enthought.com>
2019-01-08 09:33:23 +0000

    Moved install back to a single function, plus flake8 fixes

--- etstool.py
+++ etstool.py
@@ -106,8 +106,11 @@ def cli():
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--environment', default=None)
-def install(runtime, environment):
-    """ Install project and dependencies into a clean EDM environment.
+@click.option('--docs/--no-docs', default=True)
+def install(runtime, environment, docs):
+    """ Install project and dependencies into a clean EDM environment and
+    optionally install further dependencies required for building
+    documentation.
 
     """
     parameters = get_parameters(runtime, environment)
@@ -120,27 +123,15 @@ def install(runtime, environment):
     ]
     click.echo("Creating environment '{environment}'".format(**parameters))
     execute(commands, parameters)
+    if docs:
+        commands = [
+            "edm run -e {environment} -- pip install -r "
+            "ci-doc-requirements.txt --no-dependencies"
+        ]
+        execute(commands, parameters)
+        click.echo("Installed enthought-sphinx-theme in '"
+                   "{environment}'.".format(**parameters))
     click.echo('Done install')
-
-
-@cli.command(name='install-docs')
-@click.option('--runtime', default='3.6')
-@click.option('--environment', default=None)
-def install_docs(runtime, environment):
-    """ Install project and dependencies into a clean EDM environment, then
-    install further dependencies required for building documentation.
-
-    """
-    parameters = get_parameters(runtime, environment)
-    args = ['--runtime={}'.format(runtime)]
-    install(args=args, standalone_mode=False)
-    commands = [
-        "edm run -e {environment} -- pip install -r ci-doc-requirements.txt "
-        "--no-dependencies"
-    ]
-    execute(commands, parameters)
-    click.echo("Installed enthought-sphinx-theme in '"
-               "{environment}'.".format(**parameters))
 
 
 @cli.command()
@@ -152,11 +143,12 @@ def test(runtime, environment):
     """
     parameters = get_parameters(runtime, environment)
 
-    environ =  {}
+    environ = {}
     environ['PYTHONUNBUFFERED'] = "1"
 
     commands = [
-        "edm run -e {environment} -- coverage run -p -m nose.core -v traits --nologcapture"
+        "edm run -e {environment} -- coverage run -p -m nose.core -v traits "
+        "--nologcapture"
     ]
 
     # We run in a tempdir to avoid accidentally picking up wrong traits
