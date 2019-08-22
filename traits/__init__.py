d669e8eecb16e0937e189501b98c88878b528c44
Mark Dickinson <mdickinson@enthought.com>
2019-08-22 10:41:12 +0100

    Add explanation for the ImportError branch

--- traits/__init__.py
+++ traits/__init__.py
@@ -3,6 +3,10 @@ from __future__ import absolute_import
 try:
     from traits._version import full_version as __version__
 except ImportError:
+    # If we get here, we're using a source tree that hasn't been created via
+    # the setup script. That likely also means that the ctraits extension
+    # hasn't been built, so this isn't a viable Traits installation. OTOH, it
+    # can be useful if a simple "import traits" doesn't actually fail.
     __version__ = "unknown"
 
 # Add a NullHandler so 'traits' loggers don't complain when they get used.
