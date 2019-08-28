86c2072b2e8a87c64e7dd7dd122b9dee27b18cbb
Srinath Kailasa <srinathkailasa@gmail.com>
2019-08-28 14:41:55 +0100

    Add test file and license header

--- traits/util/tests/test_import_checker.py
+++ traits/util/tests/test_import_checker.py
@@ -11,28 +11,3 @@
 #  Thanks for using Enthought open source!
 #
 # -----------------------------------------------------------------------------
-import importlib
-
-
-def import_handler(name):
-    """
-    Handle import errors
-
-    Parameters
-    ----------
-    name : Str
-        The name of the module being imported, lower case.
-
-    Returns
-    -------
-    Tuple(Bool, None/module)
-        Tuple containing a bool (whether or not a module is available) and the
-        module object itself.
-
-    """
-    try:
-        module = importlib.import_module(name)
-        return True, module
-
-    except ImportError:
-        return False, None
