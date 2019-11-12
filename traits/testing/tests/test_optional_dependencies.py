d6f8188f9ec3ff1dd6c488ca9d8050c27f6aa6ad
Mark Dickinson <mdickinson@enthought.com>
2019-11-12 07:54:18 +0000

    Remove the mutation of global state.

--- traits/testing/tests/test_optional_dependencies.py
+++ traits/testing/tests/test_optional_dependencies.py
@@ -12,30 +12,18 @@
 #
 # -----------------------------------------------------------------------------
 
-import sys
-from types import ModuleType
 import unittest
 
 from traits.testing.optional_dependencies import optional_import
 
 
-class MockModule(ModuleType):
-    """Mock module as object for testing"""
-    pass
-
-
-# Assign to sys modules to allow import
-sys.modules['mock_module'] = MockModule(name='mock')
-
-
 class TestImportHandler(unittest.TestCase):
-
     def test_import_succeeds(self):
 
-        module = optional_import('mock_module')
-        self.assertEqual(module.__name__, 'mock')
+        module = optional_import("itertools")
+        self.assertEqual(module.__name__, "itertools")
 
     def test_import_fails(self):
 
-        module = optional_import('unavailable_module')
+        module = optional_import("unavailable_module")
         self.assertIsNone(module)
