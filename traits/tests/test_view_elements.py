5968c1718308d5bf526d042cd5a07209ea9f9fd1
Corran Webster <cwebster@enthought.com>
2019-12-03 15:55:05 +0000

    Add a test for base classes and view elements.

--- traits/tests/test_view_elements.py
+++ traits/tests/test_view_elements.py
@@ -45,6 +45,22 @@ class TestViewElements(unittest.TestCase):
         self.assertEqual(view_elements.content, {'my_view': view})
         self.assertIs(view_elements, view_elements2)
 
+    def test_view_elements_parents(self):
+        from traitsui.api import View
+
+        class Model(HasTraits):
+            count = Int
+            my_view = View('count')
+
+        class ModelSubclass(Model):
+            total = Int
+            my_view = View('count', 'total')
+
+        view_elements = ModelSubclass.class_trait_view_elements()
+        parent_view_elements = Model.class_trait_view_elements()
+
+        self.assertEqual(view_elements.parents[0], parent_view_elements)
+
     def test_instance_view_definition(self):
         from traitsui.api import View
 
