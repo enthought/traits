109e789da765a68c5827a9a11cd6ff348dc51309
Mark Dickinson <mdickinson@enthought.com>
2020-01-16 10:52:59 +0000

    Replace enthought/LICENSE.txt with LICENSE.txt

--- copyright_header/copyright_header.py
+++ copyright_header/copyright_header.py
@@ -2,7 +2,7 @@
 # All rights reserved.
 #
 # This software is provided without warranty under the terms of the BSD
-# license included in enthought/LICENSE.txt and may be redistributed only
+# license included in LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 #
@@ -27,7 +27,7 @@ PRODUCT_CODE_HEADER_TEMPLATE = r"""
 # All rights reserved\.
 #
 # This software is provided without warranty under the terms of the BSD
-# license included in enthought/LICENSE\.txt and may be redistributed only
+# license included in LICENSE\.txt and may be redistributed only
 # under the conditions described in the aforementioned license\.  The license
 # is also available online at http://www\.enthought\.com/licenses/BSD\.txt
 #
