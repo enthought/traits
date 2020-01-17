f63c0ed78cfe6c52a8a7b2b8b88e5faeb348af0b
Kit Yan Choi <kchoi@enthought.com>
2020-01-17 16:32:51 +0000

    Add tests for Callable

---
+++ traits/tests/test_callable.py
@@ -0,0 +1,79 @@
+# (C) Copyright 2005-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+import unittest
+
+from traits.api import Callable, HasTraits, TraitError
+
+
+
+def function():
+    pass
+
+
+class Dummy(object):
+
+    def instance_method(self):
+        pass
+
+
+class MyCallable(HasTraits):
+
+    value = Callable()
+
+
+class MyCallableCanBeNone(HasTraits):
+
+    value = Callable(allow_none=True)
+
+
+class MyCallableCannotBeNone(HasTraits):
+
+    value = Callable(allow_none=False)
+
+
+class TestCallable(unittest.TestCase):
+
+    def test_default(self):
+        a = MyCallable()
+        self.assertIsNone(a.value)
+
+    def test_accepts_lambda(self):
+        func = lambda v: v + 1
+        a = MyCallable(value=func)
+        self.assertIs(a.value, func)
+
+    def test_accepts_type(self):
+        MyCallable(value=float)
+
+    def test_accepts_method(self):
+        dummy = Dummy()
+        MyCallable(value=dummy.instance_method)
+
+    def test_accepts_none(self):
+        a = MyCallableCanBeNone()
+        a.value = None
+
+    def test_accepts_function(self):
+        MyCallable(value=function)
+
+    def test_rejects_int(self):
+        a = MyCallable()
+
+        with self.assertRaises(TraitError) as exception_context:
+            a.value = 1
+
+        self.assertIn(
+            "must be a callable value", str(exception_context.exception))
+
+    def test_rejects_none(self):
+        with self.assertRaises(TraitError):
+            MyCallableCannotBeNone(value=None)
+            
