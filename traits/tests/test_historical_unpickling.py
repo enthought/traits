de6c4ccd34fee6f57af1f40b85a4dd82de8902c5
Mark Dickinson <mdickinson@enthought.com>
2020-01-22 18:13:44 +0000

    Open in a way that's compatible with Python 3.5

--- traits/tests/test_historical_unpickling.py
+++ traits/tests/test_historical_unpickling.py
@@ -56,5 +56,5 @@ class TestHistoricalPickles(unittest.TestCase):
         # Just test that the pickle can be unpickled.
         for pickle_path in find_test_pickles():
             with self.subTest(filename=pickle_path.name):
-                with open(pickle_path, "rb") as f:
+                with pickle_path.open("rb") as f:
                     pickle.load(f)
