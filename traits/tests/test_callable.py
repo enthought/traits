9b20c55023f607ae6ae52b5cb07fc4e2849ebf5f
Mark Dickinson <mdickinson@enthought.com>
2020-01-23 17:23:26 +0000

    Add test

--- traits/tests/test_callable.py
+++ traits/tests/test_callable.py
@@ -73,9 +73,25 @@ class TestCallable(unittest.TestCase):
         self.assertIn(
             "must be a callable value", str(exception_context.exception))
 
+    def test_callable_in_complex_trait(self):
+        a = MyCallable()
+
+        self.assertIsNone(a.callable_or_str)
+
+        acceptable_values = [pow, "pow", None, int]
+        for value in acceptable_values:
+            a.callable_or_str = value
+            self.assertEqual(a.callable_or_str, value)
+
+        unacceptable_values = [1.0, 3j, (5, 6, 7)]
+        for value in unacceptable_values:
+            old_value = a.callable_or_str
+            with self.assertRaises(TraitError):
+                a.callable_or_str = value
+            self.assertEqual(a.callable_or_str, old_value)
+
 
 class TestBaseCallable(unittest.TestCase):
-
     def test_override_validate(self):
         """ Verify `BaseCallable` can be subclassed to create new traits.
         """
