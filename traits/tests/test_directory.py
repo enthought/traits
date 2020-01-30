723ceea7a65a0dd398bb50362efd784b1b9b583e
Shoeb Mohammed <smohammed@enthought.com>
2020-01-30 16:18:36 -0600

    skip pathlib tests for python < 3.6. And cleanup.

--- traits/tests/test_directory.py
+++ traits/tests/test_directory.py
@@ -9,14 +9,17 @@
 # Thanks for using Enthought open source!
 
 import pathlib
+import sys
 from tempfile import gettempdir
 
 import unittest
 
 from traits.api import (
-    BaseDirectory, Directory, Either, HasTraits, Int, TraitError
-)
+    BaseDirectory, Directory, Either, HasTraits, Int, TraitError)
 
+
+TESTS_SKIPPED_MESSAGE = (
+    "Directory trait PathLib tests skipped for Python < 3.6")
 
 class ExampleModel(HasTraits):
     path = Directory(exists=True)
@@ -71,6 +74,7 @@ class DirectoryTestCase(unittest.TestCase):
         example_model = FastExampleModel(path=gettempdir())
         example_model.path = "."
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_fast_accepts_pathlib_dir(self):
         example_model = FastExampleModel()
         example_model.path = pathlib.Path(gettempdir())
@@ -98,7 +102,7 @@ class TestBaseDirectory(unittest.TestCase):
         foo = ExistsBaseDirectory()
 
         with self.assertRaises(TraitError):
-            foo.path = "!!!"
+            foo.path = "!!!invalid_directory"
 
     def test_rejects_valid_file_name(self):
         foo = ExistsBaseDirectory()
@@ -106,18 +110,21 @@ class TestBaseDirectory(unittest.TestCase):
         with self.assertRaises(TraitError):
             foo.path = __file__
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_accepts_valid_pathlib_dir(self):
         foo = ExistsBaseDirectory()
         foo.path = pathlib.Path(gettempdir())
 
         self.assertIsInstance(foo.path, str)
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_rejects_invalid_pathlib_dir(self):
         foo = ExistsBaseDirectory()
 
         with self.assertRaises(TraitError):
-            foo.path = pathlib.Path("!!!")
+            foo.path = pathlib.Path("!!!invalid_directory")
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_rejects_valid_pathlib_file(self):
         foo = ExistsBaseDirectory()
 
@@ -133,14 +140,15 @@ class TestBaseDirectory(unittest.TestCase):
             foo.path = 1
 
         with self.assertRaises(TraitError):
-            foo.path = b"!!!"
+            foo.path = b"!!!invalid_directory"
 
     def test_simple_accepts_any_name(self):
         """ BaseDirectory with no existence check accepts any path name.
         """
         foo = SimpleBaseDirectory()
-        foo.path = "!!!"
+        foo.path = "!!!invalid_directory"
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_simple_accepts_any_pathlib(self):
         """ BaseDirectory with no existence check accepts any pathlib path.
         """
@@ -158,8 +166,9 @@ class TestEitherWithDirectory(unittest.TestCase):
 
     def test_accepts_str(self):
         foo = IntOrDirectory()
-        foo.path = "!!!"
+        foo.path = "!!!invalid_directory"
 
+    @unittest.skipIf(sys.version_info < (3, 6), TESTS_SKIPPED_MESSAGE)
     def test_accepts_pathlib(self):
         foo = IntOrDirectory()
-        foo.path = pathlib.Path("!!!")
+        foo.path = pathlib.Path("!!!invalid_directory")
