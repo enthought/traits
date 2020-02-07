2bc5b6b02262b54805d1f2d68368c39ce577c534
Shoeb Mohammed <smohammed@enthought.com>
2020-02-07 08:50:15 -0600

    Output a better error message.

--- traits/ctraits.c
+++ traits/ctraits.c
@@ -2920,7 +2920,11 @@ trait_new(PyTypeObject *trait_type, PyObject *args, PyObject *kw)
         return trait;
     }
 
-    PyErr_SetString(TraitError, "invalid argument to trait constructor");
+    PyErr_Format(
+        TraitError, 
+        "Invalid argument to trait constructor. The argument `kind` "
+        "must be an integer between 0 and 8 but a value of %d was provided.",
+        kind)
     return NULL;
 }
 
