60a7a2775ac99c988ae8dda897e89430c06d5f6e
Midhun PM <midhun@enthought.com>
2020-02-10 14:07:39 -0600

    Add a test with a empty Callable() trait

--- traits/tests/test_callable.py
+++ traits/tests/test_callable.py
@@ -108,14 +108,18 @@ class TestCallable(unittest.TestCase):
 
         class MyNewCallable2(HasTraits):
             value = Callable(pow, allow_none=True)
+            empty_callable = Callable()
             a_non_none_union = Union(Callable(allow_none=False), Int)
             a_allow_none_union = Union(Callable(allow_none=True), Int)
 
         obj = MyNewCallable2()
         self.assertIsNotNone(obj.value)
+        self.assertIsNone(obj.empty_callable)
 
         obj.value = None
+        obj.empty_callable = None
         self.assertIsNone(obj.value)
+        self.assertIsNone(obj.empty_callable)
 
         obj.a_non_none_union = 5
         obj.a_allow_none_union = 5
