ca878da6db91acfd3f1c59cb2ec52e6d4cf432de
Kit Yan Choi <kchoi@enthought.com>
2020-03-10 13:23:43 +0000

    Revert "More refactoring"
    This reverts commit 3301474d0ac9b97821c1909cb23169a89d11862a.

--- traits/tests/test_traits_listener.py
+++ traits/tests/test_traits_listener.py
@@ -28,23 +28,51 @@ def assert_listener_item_equal(test_case, item1, item2, msg=None):
     """ Assertion function for comparing two instances of ListenerItem.
     """
 
-    names = traits_listener.ListenerItem.class_editable_traits()
-    # deal with `next` differently to avoid recursion
+    def get_msg(name, msg):
+        return "{name} mismatched. {msg}".format(
+            name=name,
+            msg="" if msg is None else msg
+        )
+
+    test_case.assertEqual(
+        item1.name, item2.name,
+        msg=get_msg("name", msg),
+    )
+    test_case.assertEqual(
+        item1.metadata_name, item2.metadata_name,
+        msg=get_msg("metadata_name", msg),
+    )
+    test_case.assertEqual(
+        item1.metadata_defined, item2.metadata_defined,
+        msg=get_msg("metadata_defined", msg),
+    )
+    test_case.assertEqual(
+        item1.is_any_trait, item2.is_any_trait,
+        msg=get_msg("is_any_trait", msg),
+    )
+    test_case.assertEqual(
+        item1.dispatch, item2.dispatch,
+        msg=get_msg("dispatch", msg),
+    )
+    test_case.assertEqual(
+        item1.notify, item2.notify,
+        msg=get_msg("notify", msg),
+    )
+    test_case.assertEqual(
+        item1.is_list_handler, item2.is_list_handler,
+        msg=get_msg("is_list_handler", msg),
+    )
+    test_case.assertEqual(
+        item1.type, item2.type,
+        msg=get_msg("type", msg),
+    )
     if item1.next is item2.next:
         # avoid recursion
         pass
     else:
         test_case.assertEqual(
             item1.next, item2.next,
-            msg="next mismatched." + (msg if msg else ""),
-        )
-    names.remove("next")
-    for name in names:
-        test_case.assertEqual(
-            getattr(item1, name), getattr(item2, name),
-            msg="{name} mismatched. {msg}".format(
-                name=name, msg=(msg if msg else "")
-            )
+            msg=get_msg("next", msg),
         )
 
 
