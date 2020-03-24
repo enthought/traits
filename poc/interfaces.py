bf37aae1abb2f21bdfce58392c21382ad5c54fd1
Kit Yan Choi <kchoi@enthought.com>
2020-03-24 14:18:55 +0000

    Expand docstring [skip ci]

--- poc/interfaces.py
+++ poc/interfaces.py
@@ -2,6 +2,9 @@ import abc
 
 
 class INotifiableObject(abc.ABC):
+    """ A notifiable object is an object which has a list to/from which
+    notifiers can be added / removed.
+    """
 
     @abc.abstractmethod
     def _notifiers(self, force_create):
@@ -9,25 +12,40 @@ class INotifiableObject(abc.ABC):
 
 
 class INotifier(abc.ABC):
+    """ Interface for a notifier.
+    """
 
     def __call__(self, object, name, old, new):
+        """ Called by ctraits. """
         raise NotImplementedError()
 
     def increment(self):
+        """ Increment an internal reference count.
+        Subclass can ignore if reference counting is not required.
+        """
         raise NotImplementedError()
 
     def decrement(self):
+        """ Decrement an internal reference count.
+        Subclass can ignore if reference counting is not required.
+        """
         raise NotImplementedError()
 
     def can_be_removed(self):
+        """ Return true if this notifier can be removed from a
+        notifiable object.
+        """
         # return true or false.
         raise NotImplementedError()
 
     def dispose(self):
-        # clean up tasks **after** this notifier is removed from
-        # the notifiable object.
+        """ Clean up tasks **after** this notifier is removed from
+        the notifiable object.
+        """
         raise NotImplementedError()
 
     def equals(self, other):
-        # Return true or false
+        """ Return true if this notifier is considered equivalent to
+        a given notifier.
+        """
         raise NotImplementedError()
