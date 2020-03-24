e98cffd966c7267de9a6ea8697a3aaecb1303050
Kit Yan Choi <kchoi@enthought.com>
2020-03-24 14:41:15 +0000

    Add more description for ListenerChangeNotifier [skip ci]

--- poc/listener_change_notifier.py
+++ poc/listener_change_notifier.py
@@ -7,14 +7,49 @@ logger = logging.getLogger(__name__)
 
 
 class ListenerChangeNotifier(INotifier):
-    """ Notifier for when a trait has changed, removes
-    'extended' listeners from the removed object, and propagates
-    the 'extended' listeners to the new object.
+    """ Notifier for maintaining nested listeners when a trait has
+    changed.
+
+    For example, when ``foo.bar.baz`` is being listened to and
+    ``foo`` is changed, this notifier is fired such that listeners
+    attached to the old ``foo`` 's nested attributes are removed,
+    and new listeners are attached to the new ``foo``.
     """
 
     def __init__(
             self, listener_callback, actual_callback, path,
             target, event_factory, dispatcher):
+        """
+        Parameters
+        ----------
+        listener_callback : callable
+            Input arguments are (event, callback, path, target, dispatcher).
+            ``event`` represents the change event that should result in
+            hooking/unhooking of listeners.
+            ``callback`` is the same as ``actual_callback`` (see below).
+            ``path``, ``target``, ``dispatcher`` are passed directly from
+            the values given to this initializer.
+            ``path`` (see ).
+            ``target`` is an notifiable object that sets the context for
+            the callback. See ``target`` in ``TraitObserverNotifier``.
+            ``dispatcher`` is a callable for dispatching the ``callback``.
+        actual_callback : callable(BaseEvent)
+            The actual callable the listeners are created for.
+            i.e. it is the callback provided by the user via ``observe``.
+        path : ListenerPath
+            Path that defines what listeners to hook/unhook.
+        target : Any
+            Sets the context for the (actual) callback. In practice, it is
+            the HasTrait instance on which the listener path is defined and
+            will be seen by users as the "owner" of the notifier.
+            Strictly speaking, this object sets the context for the notifier
+            and does not have to be a notifiable object.
+        event_factory : callable
+            Factory function for creating the event to be passed to
+            ``listener_callback``.
+        dispatcher : callable(args, kwargs)
+            Callable for dispatching the ``actual_callback``.
+        """
         self.listener_callback = listener_callback
 
         #: TODO: Does this need to be a weak ref?
