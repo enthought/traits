e7bd263053a1f26c6db3fb1205877e3a0ec6fb79
Mark Dickinson <mdickinson@enthought.com>
2020-03-26 13:39:35 +0000

    Fix another case of exception swallowing from PyObject_GenericGetAttr (#960)
    * Fix another case of exception swallowing from PyObject_GenericGetAttr
    
    * Fix unused import, unrelated spacing change
    
    * Make comment a bit more explicit

--- traits/tests/test_ctraits.py
+++ traits/tests/test_ctraits.py
@@ -279,6 +279,25 @@ class TestCTrait(unittest.TestCase):
         self.assertEqual(len(foo.bar_changed), 1)
         self.assertEqual(foo.bar_changed[0], "baz")
 
+    def test_failed_attribute_access(self):
+        ctrait = CTrait(0)
+        self.assertIsNone(ctrait.non_existent)
+
+    def test_exception_from_attribute_access(self):
+        # Regression test for enthought/traits#946.
+
+        # Danger: we're (temporarily) mutating global state here! Check that
+        # we're not touching an attribute that actually exists.
+        self.assertFalse(hasattr(CTrait, "badattr_test"))
+
+        CTrait.badattr_test = property(lambda self: 1 / 0)
+        try:
+            ctrait = CTrait(0)
+            with self.assertRaises(ZeroDivisionError):
+                ctrait.badattr_test
+        finally:
+            del CTrait.badattr_test
+
 
 class TestCTraitNotifiers(unittest.TestCase):
     """ Test calling trait notifiers and object notifiers. """
