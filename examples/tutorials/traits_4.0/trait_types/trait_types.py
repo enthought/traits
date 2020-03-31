a646a64e6b5908c4c42f0ffbda557eab2bca5ba9
Midhun PM <midhun@enthought.com>
2020-03-31 13:06:52 -0500

    Make OddInt inherit from BaseInt and not Int

--- examples/tutorials/traits_4.0/trait_types/trait_types.py
+++ examples/tutorials/traits_4.0/trait_types/trait_types.py
@@ -58,7 +58,7 @@ OddInt Redux
 Using the new style of defining traits, we can rewrite our previous **OddInt**
 example as follows::
 
-    class OddInt(Int):
+    class OddInt(BaseInt):
 
         # Define the default value:
         default_value = 1
@@ -76,16 +76,16 @@ example as follows::
 This provides the exact same functionality as the previous definition of
 **OddInt**. There are several points to make about the new definition however:
 
-- The **OddInt** class derives from **Int** (not **TraitHandler**). This
+- The **OddInt** class derives from **BaseInt** (not **TraitHandler**). This
   has several important side effects:
 
-  * **OddInt** can re-use and change any part of the **Int** class behavior
-    that it needs to. Note in this case the re-use of the **Int** class's
+  * **OddInt** can re-use and change any part of the **BaseInt** class behavior
+    that it needs to. Note in this case the re-use of the **BaseInt** class's
     *validate* method via the *super* call in **OddInt's** *validate* method.
 
-  * As a subclass of **Int**, it is related to **Int**, which can be
+  * As a subclass of **BaseInt**, it is related to **BaseInt**, which can be
     important both from a documentation and programming point of view. The
-    original definition of **OddInt** was related to **Int** only in that their
+    original definition of **OddInt** was related to **BaseInt** only in that their
     names were similar.
 
 - The default value and trait description information are declared as class
@@ -103,7 +103,7 @@ from traits.api import *
 
 
 # --[OddInt Definition]--------------------------------------------------------
-class OddInt(Int):
+class OddInt(BaseInt):
 
     # Define the default value:
     default_value = 1
