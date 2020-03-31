ed6d959975088b95d21fa125ab661ab93347fea9
Mark Dickinson <mdickinson@enthought.com>
2020-03-31 07:47:38 +0100

    Fix arg[-1] to args[-1]

--- traits/trait_types.py
+++ traits/trait_types.py
@@ -1954,9 +1954,9 @@ class BaseEnum(TraitType):
             The default value for the collection.
 
         For the static case, the ambiguity in the signatures is resolved
-        as follows: if ``args`` has length ``1`` or ``2``, ``arg[-1]`` can be
-        iterated over, and ``arg[-1]`` is not an instance of ``str``, ``bytes``
-        or ``bytearray``, then ``arg[-1]`` is assumed to give the collection
+        as follows: if ``args`` has length ``1`` or ``2``, ``args[-1]`` can be
+        iterated over, and ``args[-1]`` is not an instance of ``str``, ``bytes``
+        or ``bytearray``, then ``args[-1]`` is assumed to give the collection
         of values. Otherwise, all elements of ``args`` are assumed to be
         items in the collection. Thus the first two signatures are safe
         from ambiguity, and it's recommended to use one of these two signatures
@@ -2145,9 +2145,9 @@ class Enum(BaseEnum):
             The default value for the collection.
 
         For the static case, the ambiguity in the signatures is resolved
-        as follows: if ``args`` has length ``1`` or ``2``, ``arg[-1]`` can be
-        iterated over, and ``arg[-1]`` is not an instance of ``str``, ``bytes``
-        or ``bytearray``, then ``arg[-1]`` is assumed to give the collection
+        as follows: if ``args`` has length ``1`` or ``2``, ``args[-1]`` can be
+        iterated over, and ``args[-1]`` is not an instance of ``str``, ``bytes``
+        or ``bytearray``, then ``args[-1]`` is assumed to give the collection
         of values. Otherwise, all elements of ``args`` are assumed to be
         items in the collection. Thus the first two signatures are safe
         from ambiguity, and it's recommended to use one of these two signatures
