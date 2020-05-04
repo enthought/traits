12c987d54c67ebd63879b28911c38e892dc0258b
Corran Webster <cwebster@enthought.com>
2020-05-04 07:11:54 +0100

    First cut at a traits beginner tutorial.

---
+++ examples/tutorials/introduction/__init__.py
@@ -0,0 +1,18 @@
+"""
+Introduction to Traits
+======================
+
+This is an interactive tutorial to introduce you to the basics of
+Traits and to give you an idea of what capabilities the Traits
+libraries provide.  This tutorial assumes that you are confortable
+with the Python programming language, object oriented programming,
+and the core tools of the Python scientifc ecosystem, such as
+NumPy.
+
+The tutorial starts with an **Introduction** which gives a realistic
+example of scientific code; then talks about how traits can be used
+for **Validation**, **Initialization**, **Documentation**,
+**Observation** and **Visualization**.  This tutorial is designed
+to introduce the basics of Traits, but also to explain *why* you
+might want to use Traits in your own code.
+"""
