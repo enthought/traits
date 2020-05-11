6b7c728611b2a245ce0f9aaf463a2e5f8f4cba7d
Kit Yan Choi <kchoi@enthought.com>
2020-05-11 10:30:55 +0100

    Fix tense for removed and added

--- traits/observers/_list_change_event.py
+++ traits/observers/_list_change_event.py
@@ -59,9 +59,9 @@ def list_event_factory(trait_list, index, removed, added):
     index : int or slice
         The indices being modified by the operation.
     removed : list
-        The items to be removed.
+        The items removed from the list.
     added : list
-        The items being added to the list.
+        The items added to the list.
 
     Returns
     -------
