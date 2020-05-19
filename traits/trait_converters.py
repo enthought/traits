d93526e5461d63c86080f862cf42f86fbc3ffb6c
Ieva C <icernyte@enthought.com>
2020-05-19 13:48:20 +0100

    Update mapped_trait_for docstring

--- traits/trait_converters.py
+++ traits/trait_converters.py
@@ -110,7 +110,21 @@ def trait_for(trait):
 
 
 def mapped_trait_for(trait, name):
-    """ Returns the 'mapped trait' definition for a mapped trait.
+    """ Returns the 'mapped trait' definition for a mapped trait, the default
+        value of which is a callable that maps the value of the original trait.
+
+        Parameters
+        ----------
+        trait : ctrait.CTrait
+            A trait for which the 'mapped trait' definition is being created.
+        name : str
+            The name of the trait for which the 'mapped trait' definition is
+            being created.
+
+        Returns
+        -------
+        trait_types.Any
+            A definition of the 'mapped trait'
     """
     from .trait_types import Any
     from .constants import DefaultValue
