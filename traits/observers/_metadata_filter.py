89f90b938adb271a14e155306eaaca1187aae96e
Kit Yan Choi <kchoi@enthought.com>
2020-05-19 10:45:49 +0100

    Forgo the default value, it is not used

--- traits/observers/_metadata_filter.py
+++ traits/observers/_metadata_filter.py
@@ -33,7 +33,7 @@ class MetadataFilter:
     def __call__(self, name, trait):
         # Consistent with the current behaviour where metadata with None as
         # the value is equivalent to the metadata not having been defined.
-        return getattr(trait, self.metadata_name, None) is not None
+        return getattr(trait, self.metadata_name) is not None
 
     def __eq__(self, other):
         return (
