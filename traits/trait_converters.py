9d6597a55e5555b8963d28538f81c52766be9fec
Mark Dickinson <mdickinson@enthought.com>
2020-06-04 15:37:49 +0100

    Remove explicit Undefined check in _mapped_trait_default

--- traits/trait_converters.py
+++ traits/trait_converters.py
@@ -115,9 +115,23 @@ def trait_for(trait):
 
 
 def _mapped_trait_default(trait, name, instance):
-    value = getattr(instance, name, Undefined)
-    if value is Undefined:
-        return Undefined
+    """ Callable providing default for a shadow trait of a mapped trait pair.
+
+    Parameters
+    ----------
+    trait : CTrait
+        The principal trait of the mapped trait pair.
+    name : str
+        The name of the trait on the relevant HasTraits object.
+    instance : HasTraits
+        The HasTraits object on which the mapped trait lives.
+
+    Returns
+    -------
+    default : object
+        The default value for the shadow trait.
+    """
+    value = getattr(instance, name)
     return trait.handler.mapped_value(value)
 
 
