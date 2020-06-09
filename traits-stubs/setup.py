1b42877eeaf5d967c9862bbec35f9a6f3d399019
Mark Dickinson <mdickinson@enthought.com>
2020-06-09 16:58:39 +0100

    Remove the C language classifier

--- traits-stubs/setup.py
+++ traits-stubs/setup.py
@@ -34,7 +34,6 @@ if __name__ == "__main__":
             Operating System :: MacOS :: MacOS X
             Operating System :: Microsoft :: Windows
             Operating System :: POSIX :: Linux
-            Programming Language :: C
             Programming Language :: Python
             Programming Language :: Python :: 3
             Programming Language :: Python :: 3.5
