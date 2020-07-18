2211a58e012f64cd54a647f1e74dc06d65e06a0c
Mark Dickinson <mdickinson@enthought.com>
2020-07-18 15:36:48 +0100

    And fix the failing test

--- traits/util/trait_documenter.py
+++ traits/util/trait_documenter.py
@@ -22,14 +22,19 @@ import traceback
 
 from sphinx.ext.autodoc import ClassLevelDocumenter
 
-from ..trait_type import TraitType
-from ..has_traits import MetaHasTraits
+from traits.has_traits import MetaHasTraits
+from traits.trait_type import TraitType
+from traits.traits import generic_trait
 
 
 def _is_class_trait(name, cls):
     """ Check if the name is in the list of class defined traits of ``cls``.
     """
-    return isinstance(cls, MetaHasTraits) and name in cls.__class_traits__
+    return (
+        isinstance(cls, MetaHasTraits)
+        and name in cls.__class_traits__
+        and cls.__class_traits__[name] is not generic_trait
+    )
 
 
 class TraitDocumenter(ClassLevelDocumenter):
