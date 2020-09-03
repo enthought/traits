49c063c58d347045afd76c746464853906492eed
Sai Rahul Poruri <rporuri@enthought.com>
2020-09-03 08:27:55 +0100

    update _trait_added_observer with wrapper functions

--- traits/observation/_trait_added_observer.py
+++ traits/observation/_trait_added_observer.py
@@ -13,7 +13,7 @@ from traits.observation._has_traits_helpers import (
     object_has_named_trait,
 )
 from traits.observation._i_observer import IObserver
-from traits.observation._observe import add_or_remove_notifiers
+from traits.observation._observe import add_notifiers
 from traits.observation._observer_change_notifier import ObserverChangeNotifier
 from traits.observation._observer_graph import ObserverGraph
 from traits.observation._trait_change_event import trait_event_factory
@@ -172,13 +172,12 @@ class TraitAddedObserver:
             ),
             children=graph.children
         )
-        add_or_remove_notifiers(
+        add_notifiers(
             object=event.object,
             graph=new_graph,
             handler=handler,
             target=target,
             dispatcher=dispatcher,
-            remove=False,
         )
         # There is no mirrored action to this.
         # ``remove_trait`` does not fire a change event.
