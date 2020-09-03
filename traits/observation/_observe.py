ef001724450334b5987cd410b35aed489247b274
Sai Rahul Poruri <rporuri@enthought.com>
2020-09-03 08:03:59 +0100

    pull down _add_or_remove_notifiers

--- traits/observation/_observe.py
+++ traits/observation/_observe.py
@@ -319,6 +319,27 @@ class _AddNotifier(_AddOrRemoveNotifier):
 
                 self._processed.append((change_notifier, observable))
 
+    def _add_or_remove_notifiers(self):
+        """ Add or remove user notifiers for the objects observed by the root
+        observer.
+        """
+        if not self.graph.node.notify:
+            return
+
+        for observable in self.graph.node.iter_observables(self.object):
+
+            notifier = self.graph.node.get_notifier(
+                handler=self.handler,
+                target=self.target,
+                dispatcher=self.dispatcher,
+            )
+            if self.remove:
+                notifier.remove_from(observable)
+            else:
+                notifier.add_to(observable)
+
+            self._processed.append((notifier, observable))
+
 
 class _RemoveNotifier(_AddOrRemoveNotifier):
     def __init__(self, *, object, graph, handler, target, dispatcher):
@@ -425,3 +446,24 @@ class _RemoveNotifier(_AddOrRemoveNotifier):
                     change_notifier.add_to(observable)
 
                 self._processed.append((change_notifier, observable))
+
+    def _add_or_remove_notifiers(self):
+        """ Add or remove user notifiers for the objects observed by the root
+        observer.
+        """
+        if not self.graph.node.notify:
+            return
+
+        for observable in self.graph.node.iter_observables(self.object):
+
+            notifier = self.graph.node.get_notifier(
+                handler=self.handler,
+                target=self.target,
+                dispatcher=self.dispatcher,
+            )
+            if self.remove:
+                notifier.remove_from(observable)
+            else:
+                notifier.add_to(observable)
+
+            self._processed.append((notifier, observable))
