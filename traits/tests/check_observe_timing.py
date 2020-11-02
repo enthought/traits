35809c9ad460b81aabf49a242ca5a06de1dd4e95
Aaron Ayres <aayres@enthought.com>
2020-11-02 08:10:48 -0600

    reorder functions for readability

--- traits/tests/check_observe_timing.py
+++ traits/tests/check_observe_timing.py
@@ -97,6 +97,66 @@ def scenario1(decorator):
     return (construction_time, instantiation_time, reassign_person_name_time)
 
 
+def report1():
+    """ Print a readable benchmark report for scenario 1. """
+
+    print(
+        """
+    Scenario 1:
+        Given a subclass of HasTraits with a trait defined, e.g. name = Str(),
+        compare having a method with no decorator to one that is decorated with
+        @on_trait_change("name"), one decorated with @observe("name"), and one
+        with @observe(trait('name')).
+
+        The timing we are interested in:
+        (1) Time to import the module, i.e. the class construction.
+        (2) Time to instantiate the HasTraits object.
+        (3) Time for reassigning the trait to a new value.
+
+    Times are reported as:
+    (#): absolute_time  relative_time_to_control
+        """
+    )
+    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
+                          "(3): {2:.5f} {5:5.2f}")
+
+    control_times = scenario1('')
+    relative_control_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, control_times)
+    )
+    print("control -")
+    print(benchmark_template.format(*control_times, *relative_control_times))
+
+    on_trait_change_times = scenario1("@on_trait_change('name')")
+    relative_on_trait_change_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, on_trait_change_times)
+    )
+    print("on_trait_change('name') -")
+    print(benchmark_template.format(
+        *on_trait_change_times, *relative_on_trait_change_times))
+
+    observe_times = scenario1("@observe('name')")
+    relative_observe_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_times)
+    )
+    print("observe('name') - ")
+    print(benchmark_template.format(*observe_times, *relative_observe_times))
+
+    observe_trait_times = scenario1("@observe(trait('name'))")
+    relative_observe_trait_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_trait_times)
+    )
+    print("observe(trait('name')) - ")
+    print(benchmark_template.format(
+        *observe_trait_times, *relative_observe_trait_times))
+
+    print('-' * 80)
+
+
 # Code template strings to be timed for scenario 2
 
 # Setup for constructing subclass of HasTraits with a trait referring to an
@@ -187,6 +247,69 @@ def scenario2(decorator):
     )
 
 
+def report2():
+    """ Print a readable benchmark report for scenario 2. """
+
+    print(
+        """
+    Scenario 2
+        Given a subclass of HasTraits with a trait referring to an instance of
+        HasTraits, e.g. child = Instance(AnotherClass) where AnotherClass has a
+        name trait, compare having a method with no decorator to one that is
+        decorated with @on_trait_change("child.name"), one decorated with
+        @observe("child.name"), and one with
+        @observe(trait('child').trait('name')).
+
+        The timing we are interested in:
+        (1) Time to import the module, i.e. the class construction.
+        (2) Time to instantiate the HasTraits object.
+        (3) Time for reassigning child to a new value.
+        (4) Time for reassigning child.name to a new value.
+
+    Times are reported as:
+    (#): absolute_time  relative_time_to_control
+        """
+    )
+    benchmark_template = ("(1): {0:.5f} {4:5.2f}  (2): {1:.5f} {5:5.2f}  "
+                          "(3): {2:.5f} {6:5.2f}  (4): {3:.5f} {7:5.2f}")
+
+    control_times = scenario2('')
+    relative_control_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, control_times)
+    )
+    print("control -")
+    print(benchmark_template.format(*control_times, *relative_control_times))
+
+    on_trait_change_times = scenario2("@on_trait_change('child.name')")
+    relative_on_trait_change_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, on_trait_change_times)
+    )
+    print("on_trait_change('child.name') -")
+    print(benchmark_template.format(
+        *on_trait_change_times, *relative_on_trait_change_times))
+
+    observe_times = scenario2("@observe('child.name')")
+    relative_observe_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_times)
+    )
+    print("observe('child.name') - ")
+    print(benchmark_template.format(*observe_times, *relative_observe_times))
+
+    observe_trait_times = scenario2("@observe(trait('child').trait('name'))")
+    relative_observe_trait_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_trait_times)
+    )
+    print("observe(trait('child').trait('name')) - ")
+    print(benchmark_template.format(
+        *observe_trait_times, *relative_observe_trait_times))
+
+    print('-' * 80)
+
+
 # Code template strings to be timed for scenario 3
 
 # Construct subclass of HasTraits with a trait that is defined as Property that
@@ -250,6 +373,127 @@ def scenario3_5(property_args, cached_property):
     )
 
     return (construction_time, instantiation_time, reassign_dependee_name_time)
+
+
+def report3():
+    """ Print a readable benchmark report for scenario 3. """
+
+    print(
+        """
+    Scenario 3
+        Given a subclass of HasTraits with a trait that is defined as Property
+        that depends on a simple trait, compare having the Property be defined
+        as Property(), Property(depends_on="name"), Property(observe="name"),
+        and Property(observe=trait('name')). (Note that this is a feature
+        currently available on master only).
+
+        The timing we are interested in:
+        (1) Time to import the module, i.e. the class construction.
+        (2) Time to instantiate the HasTraits object.
+        (3) Time for changing the trait being depended-on / observed.
+
+    Times are reported as:
+    (#): absolute_time  relative_time_to_control
+        """
+    )
+    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
+                          "(3): {2:.5f} {5:5.2f}")
+
+    control_times = scenario3_5('', '')
+    relative_control_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, control_times)
+    )
+    print("control -")
+    print(benchmark_template.format(*control_times, *relative_control_times))
+
+    depends_on_times = scenario3_5("depends_on='name'", '')
+    relative_depends_on_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, depends_on_times)
+    )
+    print("depends_on='name' -")
+    print(benchmark_template.format(
+        *depends_on_times, *relative_depends_on_times))
+
+    observe_times = scenario3_5("observe='name'", '')
+    relative_observe_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_times)
+    )
+    print("observe='name' - ")
+    print(benchmark_template.format(*observe_times, *relative_observe_times))
+
+    observe_trait_times = scenario3_5("observe=trait('name')", '')
+    relative_observe_trait_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_trait_times)
+    )
+    print("observe=trait('name') - ")
+    print(benchmark_template.format(
+        *observe_trait_times, *relative_observe_trait_times))
+
+    print('-' * 80)
+
+
+def report5():
+    """ Print a readable benchmark report for scenario 5. """
+
+    print(
+        """
+    Scenario 5
+        Identical to scenario 3 only using the @chached_property decorator on
+        the property's getter method.
+
+        The timing we are interested in:
+        (1) Time to import the module, i.e. the class construction.
+        (2) Time to instantiate the HasTraits object.
+        (3) Time for changing the trait being depended-on / observed.
+
+    Times are reported as:
+    (#): absolute_time  relative_time_to_control
+        """
+    )
+    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
+                          "(3): {2:.5f} {5:5.2f}")
+
+    control_times = scenario3_5('', '@cached_property')
+    relative_control_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, control_times)
+    )
+    print("control -")
+    print(benchmark_template.format(*control_times, *relative_control_times))
+
+    depends_on_times = scenario3_5("depends_on='name'", '@cached_property')
+    relative_depends_on_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, depends_on_times)
+    )
+    print("depends_on='name' -")
+    print(benchmark_template.format(
+        *depends_on_times, *relative_depends_on_times))
+
+    observe_times = scenario3_5("observe='name'", '@cached_property')
+    relative_observe_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_times)
+    )
+    print("observe='name' - ")
+    print(benchmark_template.format(*observe_times, *relative_observe_times))
+
+    observe_trait_times = scenario3_5(
+        "observe=trait('name')", '@cached_property'
+    )
+    relative_observe_trait_times = map(
+        lambda times: times[1] / times[0],
+        zip(control_times, observe_trait_times)
+    )
+    print("observe=trait('name') - ")
+    print(benchmark_template.format(
+        *observe_trait_times, *relative_observe_trait_times))
+
+    print('-' * 80)
 
 
 # Code template strings to be timed for scenario 4
@@ -336,190 +580,6 @@ def scenario4_6(property_args, cached_property):
     )
 
 
-def report1():
-    """ Print a readable benchmark report for scenario 1. """
-
-    print(
-        """
-    Scenario 1:
-        Given a subclass of HasTraits with a trait defined, e.g. name = Str(),
-        compare having a method with no decorator to one that is decorated with
-        @on_trait_change("name"), one decorated with @observe("name"), and one
-        with @observe(trait('name')).
-
-        The timing we are interested in:
-        (1) Time to import the module, i.e. the class construction.
-        (2) Time to instantiate the HasTraits object.
-        (3) Time for reassigning the trait to a new value.
-
-    Times are reported as:
-    (#): absolute_time  relative_time_to_control
-        """
-    )
-    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
-                          "(3): {2:.5f} {5:5.2f}")
-
-    control_times = scenario1('')
-    relative_control_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, control_times)
-    )
-    print("control -")
-    print(benchmark_template.format(*control_times, *relative_control_times))
-
-    on_trait_change_times = scenario1("@on_trait_change('name')")
-    relative_on_trait_change_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, on_trait_change_times)
-    )
-    print("on_trait_change('name') -")
-    print(benchmark_template.format(
-        *on_trait_change_times, *relative_on_trait_change_times))
-
-    observe_times = scenario1("@observe('name')")
-    relative_observe_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_times)
-    )
-    print("observe('name') - ")
-    print(benchmark_template.format(*observe_times, *relative_observe_times))
-
-    observe_trait_times = scenario1("@observe(trait('name'))")
-    relative_observe_trait_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_trait_times)
-    )
-    print("observe(trait('name')) - ")
-    print(benchmark_template.format(
-        *observe_trait_times, *relative_observe_trait_times))
-
-    print('-' * 80)
-
-
-def report2():
-    """ Print a readable benchmark report for scenario 2. """
-
-    print(
-        """
-    Scenario 2
-        Given a subclass of HasTraits with a trait referring to an instance of
-        HasTraits, e.g. child = Instance(AnotherClass) where AnotherClass has a
-        name trait, compare having a method with no decorator to one that is
-        decorated with @on_trait_change("child.name"), one decorated with
-        @observe("child.name"), and one with
-        @observe(trait('child').trait('name')).
-
-        The timing we are interested in:
-        (1) Time to import the module, i.e. the class construction.
-        (2) Time to instantiate the HasTraits object.
-        (3) Time for reassigning child to a new value.
-        (4) Time for reassigning child.name to a new value.
-
-    Times are reported as:
-    (#): absolute_time  relative_time_to_control
-        """
-    )
-    benchmark_template = ("(1): {0:.5f} {4:5.2f}  (2): {1:.5f} {5:5.2f}  "
-                          "(3): {2:.5f} {6:5.2f}  (4): {3:.5f} {7:5.2f}")
-
-    control_times = scenario2('')
-    relative_control_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, control_times)
-    )
-    print("control -")
-    print(benchmark_template.format(*control_times, *relative_control_times))
-
-    on_trait_change_times = scenario2("@on_trait_change('child.name')")
-    relative_on_trait_change_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, on_trait_change_times)
-    )
-    print("on_trait_change('child.name') -")
-    print(benchmark_template.format(
-        *on_trait_change_times, *relative_on_trait_change_times))
-
-    observe_times = scenario2("@observe('child.name')")
-    relative_observe_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_times)
-    )
-    print("observe('child.name') - ")
-    print(benchmark_template.format(*observe_times, *relative_observe_times))
-
-    observe_trait_times = scenario2("@observe(trait('child').trait('name'))")
-    relative_observe_trait_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_trait_times)
-    )
-    print("observe(trait('child').trait('name')) - ")
-    print(benchmark_template.format(
-        *observe_trait_times, *relative_observe_trait_times))
-
-    print('-' * 80)
-
-
-def report3():
-    """ Print a readable benchmark report for scenario 3. """
-
-    print(
-        """
-    Scenario 3
-        Given a subclass of HasTraits with a trait that is defined as Property
-        that depends on a simple trait, compare having the Property be defined
-        as Property(), Property(depends_on="name"), Property(observe="name"),
-        and Property(observe=trait('name')). (Note that this is a feature
-        currently available on master only).
-
-        The timing we are interested in:
-        (1) Time to import the module, i.e. the class construction.
-        (2) Time to instantiate the HasTraits object.
-        (3) Time for changing the trait being depended-on / observed.
-
-    Times are reported as:
-    (#): absolute_time  relative_time_to_control
-        """
-    )
-    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
-                          "(3): {2:.5f} {5:5.2f}")
-
-    control_times = scenario3_5('', '')
-    relative_control_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, control_times)
-    )
-    print("control -")
-    print(benchmark_template.format(*control_times, *relative_control_times))
-
-    depends_on_times = scenario3_5("depends_on='name'", '')
-    relative_depends_on_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, depends_on_times)
-    )
-    print("depends_on='name' -")
-    print(benchmark_template.format(
-        *depends_on_times, *relative_depends_on_times))
-
-    observe_times = scenario3_5("observe='name'", '')
-    relative_observe_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_times)
-    )
-    print("observe='name' - ")
-    print(benchmark_template.format(*observe_times, *relative_observe_times))
-
-    observe_trait_times = scenario3_5("observe=trait('name')", '')
-    relative_observe_trait_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_trait_times)
-    )
-    print("observe=trait('name') - ")
-    print(benchmark_template.format(
-        *observe_trait_times, *relative_observe_trait_times))
-
-    print('-' * 80)
-
-
 def report4():
     """ Print a readable benchmark report for scenario 4. """
 
@@ -579,66 +639,6 @@ def report4():
         zip(control_times, observe_trait_times)
     )
     print("observe=trait('child').trait('name') - ")
-    print(benchmark_template.format(
-        *observe_trait_times, *relative_observe_trait_times))
-
-    print('-' * 80)
-
-
-def report5():
-    """ Print a readable benchmark report for scenario 5. """
-
-    print(
-        """
-    Scenario 5
-        Identical to scenario 3 only using the @chached_property decorator on
-        the property's getter method.
-
-        The timing we are interested in:
-        (1) Time to import the module, i.e. the class construction.
-        (2) Time to instantiate the HasTraits object.
-        (3) Time for changing the trait being depended-on / observed.
-
-    Times are reported as:
-    (#): absolute_time  relative_time_to_control
-        """
-    )
-    benchmark_template = ("(1): {0:.5f} {3:5.2f}  (2): {1:.5f} {4:5.2f}  "
-                          "(3): {2:.5f} {5:5.2f}")
-
-    control_times = scenario3_5('', '@cached_property')
-    relative_control_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, control_times)
-    )
-    print("control -")
-    print(benchmark_template.format(*control_times, *relative_control_times))
-
-    depends_on_times = scenario3_5("depends_on='name'", '@cached_property')
-    relative_depends_on_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, depends_on_times)
-    )
-    print("depends_on='name' -")
-    print(benchmark_template.format(
-        *depends_on_times, *relative_depends_on_times))
-
-    observe_times = scenario3_5("observe='name'", '@cached_property')
-    relative_observe_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_times)
-    )
-    print("observe='name' - ")
-    print(benchmark_template.format(*observe_times, *relative_observe_times))
-
-    observe_trait_times = scenario3_5(
-        "observe=trait('name')", '@cached_property'
-    )
-    relative_observe_trait_times = map(
-        lambda times: times[1] / times[0],
-        zip(control_times, observe_trait_times)
-    )
-    print("observe=trait('name') - ")
     print(benchmark_template.format(
         *observe_trait_times, *relative_observe_trait_times))
 
