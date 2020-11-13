a9262032852187cdbdae15c0a7924e2df3fc7b23
Aaron Ayres <aayres@enthought.com>
2020-11-13 12:12:15 -0600

    add a test for introduction function in _etsdemo_info.py

---
+++ traits/examples/tests/test_etsdemo_info.py
@@ -0,0 +1,20 @@
+# (C) Copyright 2005-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+import os
+import unittest
+
+from traits.examples._etsdemo_info import introduction
+
+class TestETSDemoInfo(unittest.TestCase):
+
+    def test_introduction(self):
+        # input to introduction is currently just a placeholder
+        response = introduction({})
+        self.assertTrue(os.path.exists(response['root']))
