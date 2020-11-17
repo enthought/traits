d58c3b6942d8aaad9871fcb264ff7a51bac1b567
Mark Dickinson <mdickinson@enthought.com>
2020-11-17 12:38:58 +0000

    Update setup.py to include NumPy dependency on Python 3.9 and remove mention of Travis CI

--- setup.py
+++ setup.py
@@ -312,13 +312,11 @@ setuptools.setup(
             "Sphinx>=2.1.0,!=3.2.0",
             # Python 3.9 exclusions:
             #
-            # * NumPy installation fails on Python 3.9 on the Ubuntu Xenial
-            #   system used by Travis CI.
-            # * PySide2 is installable but also currently not working
+            # * PySide2 is installable but currently not working
             #   on Python 3.9.
             # * Because of the above, we also exclude GUI-using packages
             #   Pyface and TraitsUI on Python 3.9.
-            "numpy;python_version<'3.9'",
+            "numpy",
             "pyface;python_version<'3.9'",
             "PySide2;python_version<'3.9'",
             "traitsui;python_version<'3.9'",
