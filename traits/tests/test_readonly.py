2a3d68ba10216f8af8a0eb9051c3a5cb0d3018be
Kit Choi <kitchoi@users.noreply.github.com>
2021-01-04 14:53:19 +0000

    Make NoDefaultSpecified and no_default_specified private (#1380)
    * Make NoDefaultSpecified and no_default_specified private
    
    * Add a test that could fail if TraitType.clone did not check for default_value
    
    * Update changelog for removing NoDefaultSpecified from the public API

---
+++ traits/tests/test_readonly.py
@@ -0,0 +1,48 @@
+# (C) Copyright 2005-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+"""
+Tests for the ReadOnly trait type.
+
+"""
+import unittest
+
+from traits.api import (
+    HasTraits,
+    ReadOnly,
+    TraitError,
+)
+
+
+class ObjectWithReadOnlyText(HasTraits):
+    """ A dummy object that set the readonly trait in __init__
+
+    There exists such usage in TraitsUI.
+    """
+
+    text = ReadOnly()
+
+    def __init__(self, text, **traits):
+        self.text = text
+        super(ObjectWithReadOnlyText, self).__init__(**traits)
+
+
+class TestReadOnlyTrait(unittest.TestCase):
+    """ Test ReadOnly TraitType. """
+
+    def test_set_readonly_trait_in_init(self):
+
+        obj = ObjectWithReadOnlyText(text="ABC")
+        self.assertEqual(obj.text, "ABC")
+
+        with self.assertRaises(TraitError):
+            obj.text = "XYZ"
+
+        self.assertEqual(obj.text, "ABC")
