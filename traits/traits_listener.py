68453ee95fbdfca8efc19b53a4c09fb205bf67f5
Mark Dickinson <mdickinson@enthought.com>
2021-09-02 12:28:28 +0100

    Add ListenerHandler docstring

--- traits/traits_listener.py
+++ traits/traits_listener.py
@@ -1194,6 +1194,23 @@ class ListenerNotifyWrapper(TraitChangeNotifyWrapper):
 
 
 class ListenerHandler(object):
+    """
+    Wrapper for trait change handlers that avoids strong references to methods.
+
+    For a bound method handler, this wrapper prevents us from holding a
+    strong reference to the object bound to that bound method. For other
+    callable handlers, we do keep a strong reference to the handler.
+
+    When called with no arguments, this object returns either the actual
+    handler, or Undefined if the handler no longer exists because the object
+    it was bound to has been garbage collected.
+
+    Parameters
+    ----------
+    handler : callable
+        Object to be called when the relevant trait or traits change.
+    """
+
     def __init__(self, handler):
         if isinstance(handler, MethodType):
             self.handler_ref = weakref.WeakMethod(handler)
