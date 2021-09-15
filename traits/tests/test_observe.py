9d32b099530e189d3de22756ffce77a844ba9011
Mark Dickinson <mdickinson@enthought.com>
2021-09-15 14:42:22 +0100

    Expand tests

--- traits/tests/test_observe.py
+++ traits/tests/test_observe.py
@@ -42,6 +42,7 @@ class TestObserveDecorator(unittest.TestCase):
 
     def test_warning_on_handler_with_bad_signature(self):
         message_regex = "should be callable with a single positional argument"
+
         with self.assertWarnsRegex(UserWarning, message_regex):
             class A(HasTraits):
                 foo = Int()
@@ -49,6 +50,40 @@ class TestObserveDecorator(unittest.TestCase):
                 @observe("foo")
                 def _do_something_when_foo_changes(self):
                     pass
+
+        with self.assertWarnsRegex(UserWarning, message_regex):
+            class A(HasTraits):
+                foo = Int()
+
+                @observe("foo")
+                def _do_something_when_foo_changes(self, **kwargs):
+                    pass
+
+    def test_decorated_method_signatures(self):
+        # Test different handler signatures for compatibility with
+        # observe decorator.
+
+        class A(HasTraits):
+            foo = Int()
+
+            call_count = Int(0)
+
+            @observe("foo")
+            def _method_with_extra_optional_args(self, event, frombicate=True):
+                self.call_count += 1
+
+            @observe("foo")
+            def _method_with_star_args(self, *args):
+                self.call_count += 1
+
+            @observe("foo")
+            def _method_with_alternative_name(self, foo_change_event):
+                self.call_count += 1
+
+        a = A()
+        self.assertEqual(a.call_count, 0)
+        a.foo = 23
+        self.assertEqual(a.call_count, 3)
 
 
 class Student(HasTraits):
