ffd6979d3a2479d6da6a68f7ef985ab151ca258a
Mark Dickinson <mdickinson@enthought.com>
2021-09-21 09:14:45 +0100

    Fix deprecation of Function and Method (#1543)

--- traits/tests/test_trait_types.py
+++ traits/tests/test_trait_types.py
@@ -112,9 +112,21 @@ class TraitTypesTest(unittest.TestCase):
 
 class TestDeprecatedTraitTypes(unittest.TestCase):
     def test_function_deprecated(self):
+        def some_function():
+            pass
+
         with self.assertWarnsRegex(DeprecationWarning, "Function trait type"):
             Function()
+        with self.assertWarnsRegex(DeprecationWarning, "Function trait type"):
+            Function(some_function, washable=True)
 
     def test_method_deprecated(self):
+
+        class A:
+            def some_method(self):
+                pass
+
         with self.assertWarnsRegex(DeprecationWarning, "Method trait type"):
             Method()
+        with self.assertWarnsRegex(DeprecationWarning, "Method trait type"):
+            Method(A().some_method, gluten_free=False)
