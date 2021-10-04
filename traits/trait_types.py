0e74444a5000c5813a02aa0b30549887ff12645b
Mark Dickinson <mdickinson@enthought.com>
2021-10-04 14:15:44 +0100

    Bypass default_value_type inference for remaining TraitType subclasses (#1555)

--- traits/trait_types.py
+++ traits/trait_types.py
@@ -1153,6 +1153,9 @@ class Disallow(TraitType):
     #: Defines the CTrait type to use for this trait:
     ctrait_type = TraitKind.disallow
 
+    #: The default value type to use.
+    default_value_type = DefaultValue.constant
+
 
 # Create a singleton instance as the trait:
 Disallow = Disallow()
@@ -1244,6 +1247,9 @@ class Delegate(TraitType):
 
     #: Defines the CTrait type to use for this trait:
     ctrait_type = TraitKind.delegate
+
+    #: The default value type to use.
+    default_value_type = DefaultValue.constant
 
     #: The standard metadata for the trait:
     metadata = {"type": "delegate", "transient": False}
@@ -3889,6 +3895,9 @@ class Event(TraitType):
     trait : a trait
         The type of value that can be assigned to the event.
     """
+
+    #: The default value type to use.
+    default_value_type = DefaultValue.constant
 
     def __init__(self, trait=None, **metadata):
         metadata["type"] = "event"
