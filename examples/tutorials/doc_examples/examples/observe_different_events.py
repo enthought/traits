3e22388e60acca6bb138f54fee06e6f34009c1f3
Mark Dickinson <mdickinson@enthought.com>
2022-01-31 10:19:28 +0000

    Update copyright headers (#1612)

--- examples/tutorials/doc_examples/examples/observe_different_events.py
+++ examples/tutorials/doc_examples/examples/observe_different_events.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2005-2021 Enthought, Inc., Austin, TX
+# (C) Copyright 2005-2022 Enthought, Inc., Austin, TX
 # All rights reserved.
 #
 # This software is provided without warranty under the terms of the BSD
