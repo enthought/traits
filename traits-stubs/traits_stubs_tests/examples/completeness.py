7a71695f215e21494c5f4fa18df840e412074987
Mark Dickinson <mdickinson@enthought.com>
2022-06-29 11:59:01 +0100

    Fix spacing for type hints

--- traits-stubs/traits_stubs_tests/examples/completeness.py
+++ traits-stubs/traits_stubs_tests/examples/completeness.py
@@ -19,7 +19,7 @@ class HasName(HasStrictTraits):
     name = Str()
 
 
-def try_assigning_age(x : HasName, new_name : str):
+def try_assigning_age(x: HasName, new_name: str):
     try:
         x.age = new_name
     except TraitError:
